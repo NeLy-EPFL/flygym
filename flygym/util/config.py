674a776886318ad6a9554f7490cb5fb791674612
Sibo Wang <sibo.wang@epfl.ch>
2023-04-25 08:44:53 +0200

    remove tarsus2-5 from all_leg_joints, deprecate fused tarsus def

--- flygym/util/config.py
+++ flygym/util/config.py
@@ -4,12 +4,7 @@ all_leg_dofs = [
     for side in 'LR'
     for pos in 'FMH'
     for dof in ['Coxa', 'Coxa_roll', 'Coxa_yaw',
-                'Femur', 'Femur_roll', 'Tibia',
-                'Tarsus1', 'Tarsus2', 'Tarsus3', 'Tarsus4', 'Tarsus5']
-]
-leg_dofs_fused_tarsi = [
-    dof for dof in all_leg_dofs
-    if ('Tarsus' not in dof) or dof.endswith('Tarsus1')
+                'Femur', 'Femur_roll', 'Tibia', 'Tarsus1']
 ]
 leg_dofs_3_per_leg = [
     f'joint_{side}{pos}{dof}'
