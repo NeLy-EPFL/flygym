1d8d731c95b8a253ae6253ac7ee3e627b0fd2991
Sibo Wang <sibo.wang@epfl.ch>
2023-06-05 15:02:43 +0200

    apply Black; see #23

--- flygym/terrain/base.py
+++ flygym/terrain/base.py
@@ -5,7 +5,7 @@ from typing import Tuple, List, Dict, Any
 
 class BaseTerrain(ABC):
     arena = None
-    
+
     @abstractmethod
     def __init__(self, *args: List, **kwargs: Dict):
         """Create a new terrain object.
@@ -17,12 +17,11 @@ class BaseTerrain(ABC):
             what it is depends on the physics simulator.
         """
         pass
-        
+
     @abstractmethod
-    def get_spawn_position(self,
-                           rel_pos: np.ndarray,
-                           rel_angle: np.ndarray
-                           ) -> Tuple[np.ndarray, np.ndarray]:
+    def get_spawn_position(
+        self, rel_pos: np.ndarray, rel_angle: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray]:
         """Given a relative entity spawn position and orientation (as
         if it was a simple flat terrain), return the abjusted position
         and orientation. This is useful for environments that have
@@ -48,11 +47,10 @@ class BaseTerrain(ABC):
             Adjusted axis-angle representation (x, y, z, a).
         """
         pass
-    
-    def spawn_entity(self,
-                     entity: Any,
-                     rel_pos: np.ndarray,
-                     rel_angle: np.ndarray) -> None:
+
+    def spawn_entity(
+        self, entity: Any, rel_pos: np.ndarray, rel_angle: np.ndarray
+    ) -> None:
         """Add an entity (eg. the fly) to the arena.
 
         Parameters
@@ -69,6 +67,5 @@ class BaseTerrain(ABC):
             a is the rotation angle in unit as configured in the model.
         """
         adj_pos, adj_angle = self.get_spawn_position(rel_pos, rel_angle)
-        spawn_site = self.arena.worldbody.add('site',
-                                              pos=adj_pos, axisangle=adj_angle)
-        spawn_site.attach(entity).add('freejoint')
+        spawn_site = self.arena.worldbody.add("site", pos=adj_pos, axisangle=adj_angle)
+        spawn_site.attach(entity).add("freejoint")
