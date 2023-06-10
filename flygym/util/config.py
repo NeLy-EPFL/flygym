c537ffc77220bfa9a2929f92029a43997f9ab4cb
Sibo Wang <sibo.wang@epfl.ch>
2023-06-10 15:02:05 +0200

    refactor sensor definition into separate methods

--- flygym/util/config.py
+++ flygym/util/config.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 # DoF definitions
 all_leg_dofs = [
     f"joint_{side}{pos}{dof}"
@@ -22,33 +25,47 @@ leg_dofs_3_per_leg = [
 
 
 # Geometries
-all_tarsi_collisions_geoms = [
-    f"{side}{pos}{dof}_collision"
-    for side in "LR"
-    for pos in "FMH"
-    for dof in ["Tarsus1", "Tarsus2", "Tarsus3", "Tarsus4", "Tarsus5"]
-]
 all_tarsi_links = ["Tarsus1", "Tarsus2", "Tarsus3", "Tarsus4", "Tarsus5"]
 
-all_legs_collisions_geoms = [
-    f"{side}{pos}{dof}_collision"
-    for side in "LR"
-    for pos in "FMH"
-    for dof in [
-        "Coxa",
-        "Femur",
-        "Tibia",
-        "Tarsus1",
-        "Tarsus2",
-        "Tarsus3",
-        "Tarsus4",
-        "Tarsus5",
-    ]
-]
 
-all_legs_collisions_geoms_no_coxa = [
-    f"{side}{pos}{dof}_collision"
-    for side in "LR"
-    for pos in "FMH"
-    for dof in ["Femur", "Tibia", "Tarsus1", "Tarsus2", "Tarsus3", "Tarsus4", "Tarsus5"]
-]
+def get_collision_geoms(config: str = "all") -> List[str]:
+    if config == "legs":
+        return [
+            f"{side}{pos}{dof}_collision"
+            for side in "LR"
+            for pos in "FMH"
+            for dof in [
+                "Coxa",
+                "Femur",
+                "Tibia",
+                "Tarsus1",
+                "Tarsus2",
+                "Tarsus3",
+                "Tarsus4",
+                "Tarsus5",
+            ]
+        ]
+    elif config == "legs-no-coxa":
+        return [
+            f"{side}{pos}{dof}_collision"
+            for side in "LR"
+            for pos in "FMH"
+            for dof in [
+                "Femur",
+                "Tibia",
+                "Tarsus1",
+                "Tarsus2",
+                "Tarsus3",
+                "Tarsus4",
+                "Tarsus5",
+            ]
+        ]
+    elif config == "tarsi":
+        return [
+            f"{side}{pos}{dof}_collision"
+            for side in "LR"
+            for pos in "FMH"
+            for dof in ["Tarsus1", "Tarsus2", "Tarsus3", "Tarsus4", "Tarsus5"]
+        ]
+    else:
+        raise ValueError(f"Unknown collision geometry configuration: {config}")
