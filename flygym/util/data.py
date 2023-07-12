7e1bda5e24148616e045563eb2de2b86ad0e4e63
stimpfli <victor.stimpfling@epfl.ch>
2023-07-12 16:54:33 +0200

    should not have supressed the file
    This reverts commit 2430bc8dc8b17ba58725a415b68d19915e2371cc.

---
+++ flygym/util/data.py
@@ -0,0 +1,30 @@
+import pkg_resources as _pkg_resources
+from pathlib import Path as _Path
+
+
+data_path = _Path(_pkg_resources.resource_filename("flygym", "data"))
+if not data_path.is_dir():
+    raise FileNotFoundError(
+        f"Data directory not found (expected at {data_path}). "
+        "Please reinstall the package."
+    )
+
+# MuJoCo
+mujoco_groundwalking_model_path = (
+    data_path / "mjcf/groundwalking_nmf_mjcf_nofloor_230518__bendTarsus_scaled.xml"
+)
+
+# Isaac Gym
+...
+
+# PyBullet
+...
+
+# Pose
+default_pose_path = data_path / "pose/pose_default.yaml"
+stretch_pose_path = data_path / "pose/pose_stretch.yaml"
+zero_pose_path = data_path / "pose/pose_zero.yaml"
+
+# Textures
+
+texture_images_path = data_path / "texture_files"
