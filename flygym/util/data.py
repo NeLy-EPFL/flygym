63c4add17351789975fd0d1cd15a119f2a033c5e
stimpfli <victor.stimpfling@epfl.ch>
2023-07-12 17:10:01 +0200

    cleaning

--- flygym/util/data.py
+++ flygym/util/data.py
@@ -24,4 +24,3 @@ mujoco_groundwalking_model_path = (
 default_pose_path = data_path / "pose/pose_default.yaml"
 stretch_pose_path = data_path / "pose/pose_stretch.yaml"
 zero_pose_path = data_path / "pose/pose_zero.yaml"
-
