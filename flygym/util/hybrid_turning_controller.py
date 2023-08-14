4f71bf6d8cf36ba58e720de6e43c4d9cf3c5d38d
bloues42 <48278430+lou-42@users.noreply.github.com>
2023-08-14 17:22:37 +0200

    adhesion turning

--- flygym/util/hybrid_turning_controller.py
+++ flygym/util/hybrid_turning_controller.py
@@ -10,7 +10,7 @@ from tqdm import trange
 from flygym.util.config import all_leg_dofs
 from flygym.state import stretched_pose
 
-from cpg_controller import CPG
+from flygym.util.cpg_controller import CPG
 
 from gymnasium import spaces
 
@@ -40,6 +40,7 @@ class NMFHybridTurning(NeuroMechFlyMuJoCo):
         self._load_preprogrammed_stepping()
 
         # Initialization of the rule-based part of the controller
+        self._init_raise_leg()
         self._reset_leg_retraction_state()
         self._reset_stumble_state()
         self.enable_rules_delay = 500
@@ -63,13 +64,28 @@ class NMFHybridTurning(NeuroMechFlyMuJoCo):
 
         # Compute joint positions from NN output
         joints_action = self.compute_joints_cpg(action)
-        if self.timer > self.enable_rules_delay:
-            # Add action of rules
-            joints_action += self.leg_retraction_rule(obs["end_effectors"][2::3]) 
-            joints_action += self.stumble_rule()
 
-        # Get adhesion signal
-        adhesion_signal = np.zeros(6)
+        # if self.timer > self.enable_rules_delay:
+        #     # Updating rules' effect and adding them to the action
+        #     joints_action += self.increment_leg_retraction_rule(
+        #         obs["end_effectors"][2::3]
+        #     )
+        #     joints_action += self.increment_stumble_rule(
+        #         obs["contact_forces"][::2, self.leg_tarsus1T_contactsensors]
+        #     )
+
+        if self.sim_params.enable_adhesion:
+            # Get adhesion signal
+            adhesion_signal = self.get_adhesion_vector()
+            # If leg in an hole or contacting with the wrong part of the leg
+            # remove adhesion
+            # adhesion_signal[
+            #     np.logical_or(self.legs_in_hole, self.highest_proximal_contact_leg)[
+            #         self.last_tarsalseg_to_adh_id
+            #     ]
+            # ] = 0.0
+        else:
+            adhesion_signal = np.zeros(6)
 
         self.timer +=1
 
@@ -91,7 +107,7 @@ class NMFHybridTurning(NeuroMechFlyMuJoCo):
 
         return joints_action
     
-    def leg_retraction_rule(self, ee_z_pos):
+    def increment_leg_retraction_rule(self, ee_z_pos):
         # Detect legs in hole, keep only the deepest
         legs_in_hole = ee_z_pos < self.floor_height
         legs_in_hole = np.logical_and(legs_in_hole, ee_z_pos == np.min(ee_z_pos))
@@ -102,12 +118,31 @@ class NMFHybridTurning(NeuroMechFlyMuJoCo):
             else:
                 if self.legs_in_hole_increment[k] > 0:
                     self.legs_in_hole_increment[k] -= self.decrease_rate
-                    
-        return self.legs_in_hole_increment
-    
-    def stumble_rule(self):
-        return 0
 
+        return (self.raise_leg.T * self.legs_in_hole_increment).sum(axis=1)
+    
+    def increment_stumble_rule(self, tarsus1_contacts):
+        # Compute forces
+        tarsus1T_contact_force = np.mean(
+            np.abs(tarsus1_contacts),
+            axis=(0, -1),
+        )
+
+        # Keep the highest force
+        highest_proximal_contact_leg = np.logical_and(
+            tarsus1T_contact_force > self.force_threshold,
+            max(tarsus1T_contact_force) == tarsus1T_contact_force,
+        )
+
+        for k, tarsal_seg in enumerate(self.last_tarsalseg_names):
+            if highest_proximal_contact_leg[k] and not self.legs_in_hole[k]:
+                self.legs_w_proximalcontact_increment[k] += self.increase_rate
+            else:
+                if self.legs_w_proximalcontact_increment[k] > 0:
+                    self.legs_w_proximalcontact_increment[k] -= self.decrease_rate
+
+        return (self.raise_leg.T * self.legs_w_proximalcontact_increment).sum(axis=1)
+        
     def _load_preprogrammed_stepping(self):
         legs = ["LF", "LM", "LH", "RF", "RM", "RH"]
         n_joints = len(self.actuated_joints)
@@ -200,3 +235,30 @@ class NMFHybridTurning(NeuroMechFlyMuJoCo):
             for i, lts in enumerate(self.last_tarsalseg_names)
             if lts[:2] == adh.name[:2]
         ]
+
+    def _init_raise_leg(self):
+        legs = [lts[:2] for lts in self.last_tarsalseg_names]
+        joints_in_leg = [
+            [i for i, joint in enumerate(self.actuated_joints) if leg in joint]
+            for leg in legs
+        ]
+        joint_name_to_id = {
+            joint[8:]: i
+            for i, joint in enumerate(self.actuated_joints[: len(joints_in_leg[0])])
+        }
+        raise_leg = np.zeros((len(legs), 42))
+        for i, leg in enumerate(legs):
+            if "F" in leg:
+                raise_leg[i, joints_in_leg[i][joint_name_to_id["Femur"]]] = -0.02
+                raise_leg[i, joints_in_leg[i][joint_name_to_id["Tibia"]]] = +0.016
+            elif "M" in leg:
+                raise_leg[i, joints_in_leg[i][joint_name_to_id["Coxa"]]] = -0.015
+                raise_leg[i, joints_in_leg[i][joint_name_to_id["Femur"]]] = 0.004
+                raise_leg[i, joints_in_leg[i][joint_name_to_id["Tibia"]]] = 0.01
+                raise_leg[i, joints_in_leg[i][joint_name_to_id["Tarsus1"]]] = -0.008
+                raise_leg[i, joints_in_leg[i]] *= 1.2
+            elif "H" in leg:
+                raise_leg[i, joints_in_leg[i][joint_name_to_id["Femur"]]] = -0.01
+                raise_leg[i, joints_in_leg[i][joint_name_to_id["Tibia"]]] = +0.005
+        
+        self.raise_leg = raise_leg
