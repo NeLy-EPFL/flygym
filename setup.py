f7c1a4ae697729ee21c082f66bc7af4d89cecb5d
Sibo Wang <sibo.wang@epfl.ch>
2024-01-29 15:54:09 +0100

    change version to 0.2.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flygym",
-    version="0.2.4",
+    version="0.2.5",
     author="Neuroengineering Laboratory, EPFL",
     author_email="sibo.wang@epfl.ch",
     description="Gym environments for NeuroMechFly in various physics simulators",
