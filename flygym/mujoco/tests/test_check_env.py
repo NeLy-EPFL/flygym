d8362449d4c887353b3ff4486b55f2b0ca20551f
tlam <thomas.lam@epfl.ch>
2024-02-19 22:24:19 +0100

    restore tests

--- flygym/mujoco/tests/test_check_env.py
+++ flygym/mujoco/tests/test_check_env.py
@@ -1,52 +1,52 @@
-# import pytest
-# import gymnasium.spaces as spaces
-# import gymnasium.utils.env_checker as env_checker
-#
-# from flygym.mujoco import NeuroMechFly, Parameters
-# from flygym.mujoco.arena import OdorArena
-#
-#
-# def test_check_env_basic():
-#     sim_params = Parameters(enable_vision=False, enable_olfaction=False)
-#     nmf = NeuroMechFly(sim_params=sim_params)
-#     # Override action space so joint control signals are reasonably bound. Otherwise,
-#     # the physics might become invalid
-#     nmf.action_space = spaces.Dict(
-#         {
-#             "joints": spaces.Box(low=-0.1, high=0.1, shape=(len(nmf.actuated_joints),)),
-#             "adhesion": spaces.Discrete(n=2, start=0),  # 0: no adhesion, 1: adhesion
-#         }
-#     )
-#     env_checker.check_env(nmf, skip_render_check=True)
-#
-#
-# @pytest.mark.skip(
-#     reason="github actions runner doesn't have a display; render will fail"
-# )
-# def test_check_env_vision():
-#     sim_params = Parameters(enable_vision=True, enable_olfaction=False)
-#     nmf = NeuroMechFly(sim_params=sim_params)
-#     # Override action space so joint control signals are reasonably bound. Otherwise,
-#     # the physics might become invalid
-#     nmf.action_space = spaces.Dict(
-#         {
-#             "joints": spaces.Box(low=-0.1, high=0.1, shape=(len(nmf.actuated_joints),)),
-#             "adhesion": spaces.Discrete(n=2, start=0),  # 0: no adhesion, 1: adhesion
-#         }
-#     )
-#     env_checker.check_env(nmf, skip_render_check=True)
-#
-#
-# def test_check_env_olfaction():
-#     sim_params = Parameters(enable_vision=False, enable_olfaction=True)
-#     arena = OdorArena(odor_source=[[10, 0, 0]], peak_odor_intensity=[[1, 2]])
-#     nmf = NeuroMechFly(sim_params=sim_params, arena=arena)
-#     # Override action space so joint control signals are reasonably bound. Otherwise,
-#     # the physics might become invalid
-#     nmf.action_space = spaces.Dict(
-#         {
-#             "joints": spaces.Box(low=-0.1, high=0.1, shape=(len(nmf.actuated_joints),)),
-#             "adhesion": spaces.Discrete(n=2, start=0),  # 0: no adhesion, 1: adhesion
-#         }
-#     )
-#     env_checker.check_env(nmf, skip_render_check=True)
+import pytest
+import gymnasium.spaces as spaces
+import gymnasium.utils.env_checker as env_checker
+
+from flygym.mujoco import NeuroMechFly, Parameters
+from flygym.mujoco.arena import OdorArena
+
+
+def test_check_env_basic():
+    sim_params = Parameters(enable_vision=False, enable_olfaction=False)
+    nmf = NeuroMechFly(sim_params=sim_params)
+    # Override action space so joint control signals are reasonably bound. Otherwise,
+    # the physics might become invalid
+    nmf.action_space = spaces.Dict(
+        {
+            "joints": spaces.Box(low=-0.1, high=0.1, shape=(len(nmf.actuated_joints),)),
+            "adhesion": spaces.Discrete(n=2, start=0),  # 0: no adhesion, 1: adhesion
+        }
+    )
+    env_checker.check_env(nmf, skip_render_check=True)
+
+
+@pytest.mark.skip(
+    reason="github actions runner doesn't have a display; render will fail"
+)
+def test_check_env_vision():
+    sim_params = Parameters(enable_vision=True, enable_olfaction=False)
+    nmf = NeuroMechFly(sim_params=sim_params)
+    # Override action space so joint control signals are reasonably bound. Otherwise,
+    # the physics might become invalid
+    nmf.action_space = spaces.Dict(
+        {
+            "joints": spaces.Box(low=-0.1, high=0.1, shape=(len(nmf.actuated_joints),)),
+            "adhesion": spaces.Discrete(n=2, start=0),  # 0: no adhesion, 1: adhesion
+        }
+    )
+    env_checker.check_env(nmf, skip_render_check=True)
+
+
+def test_check_env_olfaction():
+    sim_params = Parameters(enable_vision=False, enable_olfaction=True)
+    arena = OdorArena(odor_source=[[10, 0, 0]], peak_odor_intensity=[[1, 2]])
+    nmf = NeuroMechFly(sim_params=sim_params, arena=arena)
+    # Override action space so joint control signals are reasonably bound. Otherwise,
+    # the physics might become invalid
+    nmf.action_space = spaces.Dict(
+        {
+            "joints": spaces.Box(low=-0.1, high=0.1, shape=(len(nmf.actuated_joints),)),
+            "adhesion": spaces.Discrete(n=2, start=0),  # 0: no adhesion, 1: adhesion
+        }
+    )
+    env_checker.check_env(nmf, skip_render_check=True)
--- flygym/mujoco/tests/test_vision.py
+++ flygym/mujoco/tests/test_vision.py
@@ -1,65 +1,65 @@
-# import numpy as np
-# import tempfile
-# import pytest
-# import logging
-# from pathlib import Path
-#
-# from flygym.mujoco import NeuroMechFly, Parameters
-# from flygym.mujoco.util import load_config
-# from flygym.mujoco.vision import visualize_visual_input
-#
-#
-# np.random.seed(0)
-#
-#
-# @pytest.mark.skip(
-#     reason="github actions runner doesn't have a display; render will fail"
-# )
-# def test_vision_dimensions():
-#     # Load config
-#     config = load_config()
-#
-#     # Initialize simulation
-#     num_steps = 100
-#     sim_params = Parameters(
-#         enable_olfaction=True, enable_vision=True, render_raw_vision=True
-#     )
-#     nmf = NeuroMechFly(sim_params=sim_params)
-#
-#     # Run simulation
-#     obs_list = []
-#     info_list = []
-#     for i in range(num_steps):
-#         joint_pos = np.zeros(len(nmf.actuated_joints))
-#         action = {"joints": joint_pos}
-#         obs, reward, terminated, truncated, info = nmf.step(action)
-#         # nmf.render()
-#         obs_list.append(obs)
-#         info_list.append(info)
-#     nmf.close()
-#
-#     # Check dimensionality
-#     assert len(obs_list) == num_steps
-#     assert nmf.vision_update_mask.shape == (num_steps,)
-#     assert nmf.vision_update_mask.sum() == int(
-#         num_steps * sim_params.timestep * sim_params.vision_refresh_rate
-#     )
-#     height = config["vision"]["raw_img_height_px"]
-#     width = config["vision"]["raw_img_width_px"]
-#     assert info["raw_vision"].shape == (2, height, width, 3)
-#     assert obs["vision"].shape == (2, config["vision"]["num_ommatidia_per_eye"], 2)
-#
-#     print((obs["vision"][:, :, 0] > 0).sum(), (obs["vision"][:, :, 1] > 0).sum())
-#
-#     # Test postprocessing
-#     temp_base_dir = Path(tempfile.gettempdir()) / "flygym_test"
-#     logging.info(f"temp_base_dir: {temp_base_dir}")
-#     visualize_visual_input(
-#         nmf.retina,
-#         output_path=temp_base_dir / "vision/eyes.mp4",
-#         vision_data_li=[x["vision"] for x in obs_list],
-#         raw_vision_data_li=[x["raw_vision"] for x in info_list],
-#         vision_update_mask=nmf.vision_update_mask,
-#         vision_refresh_rate=sim_params.vision_refresh_rate,
-#         playback_speed=0.1,
-#     )
+import numpy as np
+import tempfile
+import pytest
+import logging
+from pathlib import Path
+
+from flygym.mujoco import NeuroMechFly, Parameters
+from flygym.mujoco.util import load_config
+from flygym.mujoco.vision import visualize_visual_input
+
+
+np.random.seed(0)
+
+
+@pytest.mark.skip(
+    reason="github actions runner doesn't have a display; render will fail"
+)
+def test_vision_dimensions():
+    # Load config
+    config = load_config()
+
+    # Initialize simulation
+    num_steps = 100
+    sim_params = Parameters(
+        enable_olfaction=True, enable_vision=True, render_raw_vision=True
+    )
+    nmf = NeuroMechFly(sim_params=sim_params)
+
+    # Run simulation
+    obs_list = []
+    info_list = []
+    for i in range(num_steps):
+        joint_pos = np.zeros(len(nmf.actuated_joints))
+        action = {"joints": joint_pos}
+        obs, reward, terminated, truncated, info = nmf.step(action)
+        # nmf.render()
+        obs_list.append(obs)
+        info_list.append(info)
+    nmf.close()
+
+    # Check dimensionality
+    assert len(obs_list) == num_steps
+    assert nmf.vision_update_mask.shape == (num_steps,)
+    assert nmf.vision_update_mask.sum() == int(
+        num_steps * sim_params.timestep * sim_params.vision_refresh_rate
+    )
+    height = config["vision"]["raw_img_height_px"]
+    width = config["vision"]["raw_img_width_px"]
+    assert info["raw_vision"].shape == (2, height, width, 3)
+    assert obs["vision"].shape == (2, config["vision"]["num_ommatidia_per_eye"], 2)
+
+    print((obs["vision"][:, :, 0] > 0).sum(), (obs["vision"][:, :, 1] > 0).sum())
+
+    # Test postprocessing
+    temp_base_dir = Path(tempfile.gettempdir()) / "flygym_test"
+    logging.info(f"temp_base_dir: {temp_base_dir}")
+    visualize_visual_input(
+        nmf.retina,
+        output_path=temp_base_dir / "vision/eyes.mp4",
+        vision_data_li=[x["vision"] for x in obs_list],
+        raw_vision_data_li=[x["raw_vision"] for x in info_list],
+        vision_update_mask=nmf.vision_update_mask,
+        vision_refresh_rate=sim_params.vision_refresh_rate,
+        playback_speed=0.1,
+    )
