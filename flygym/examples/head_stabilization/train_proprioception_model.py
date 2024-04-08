505be0a15731647ee9cc0e0ad2ca56cdc07f15fc
Sibo Wang <sibo.wang@epfl.ch>
2024-04-08 14:18:58 +0200

    train model on complex terrain - feature selection pending

--- flygym/examples/head_stabilization/train_proprioception_model.py
+++ flygym/examples/head_stabilization/train_proprioception_model.py
@@ -5,6 +5,7 @@ import lightning as pl
 import pickle
 from torch.utils.data import DataLoader, ConcatDataset, random_split
 from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
 from sklearn.metrics import r2_score
 from pathlib import Path
 from copy import deepcopy
@@ -12,6 +13,7 @@ from tqdm import tqdm
 
 import flygym
 from flygym.examples.head_stabilization import WalkingDataset, ThreeLayerMLP
+
 import flygym.examples.head_stabilization.viz as viz
 
 
@@ -19,9 +21,9 @@ import flygym.examples.head_stabilization.viz as viz
 base_dir = sim_data_dir = Path(
     "/home/sibwang/Projects/flygym/outputs/head_stabilization/"
 )
-retrain_base = True
+retrain_base = False
 retrain_feature_selection = True
-max_epochs = 3
+max_epochs = 50
 
 
 # Setup paths etc
@@ -35,34 +37,54 @@ pl.pytorch.seed_everything(0, workers=True)
 torch.set_float32_matmul_precision("medium")
 
 
+# Get joint angle scaler (use any one dataset as this doesn't have to be precise)
+_ds = WalkingDataset(
+    base_dir / "random_exploration/tripod_flat_train_set_1.00_1.00/sim_data.pkl"
+)
+joint_angle_scaler = _ds.joint_angle_scaler
+
+
 # Load datasets
-sim_data_pickles = {
-    gait: sim_data_dir / "random_exploration" / gait / "sim_data.pkl"
-    for gait in ["tripod", "tetrapod", "wave"]
-}
-datasets = {}
-joint_angle_scaler = None
-for gait, path in sim_data_pickles.items():
-    ds = WalkingDataset(path, joint_angle_scaler=joint_angle_scaler)
-    joint_angle_scaler = ds.joint_angle_scaler
-    datasets[gait] = ds
-with open(sim_data_dir / "models/joint_angle_scaler_params.pkl", "wb") as f:
-    pickle.dump({"mean": joint_angle_scaler.mean, "std": joint_angle_scaler.std}, f)
+individual_datasets = {}
+concat_datasets = {}
+for subset in ["train", "test"]:
+    li = []
+    individual_datasets[subset] = {}
+    for gait in ["tripod", "tetrapod", "wave"]:
+        individual_datasets[subset][gait] = {}
+        for terrain in ["flat", "blocks"]:
+            individual_datasets[subset][gait][terrain] = {}
+            paths = base_dir.glob(f"random_exploration/{gait}_{terrain}_{subset}_set_*")
+            dn_drives = ["_".join(p.name.split("_")[-2:]) for p in paths]
+            for dn_drive in dn_drives:
+                trial = f"{gait}_{terrain}_{subset}_set_{dn_drive}"
+                path = base_dir / f"random_exploration/{trial}/sim_data.pkl"
+                ds = WalkingDataset(path, joint_angle_scaler=joint_angle_scaler)
+                individual_datasets[subset][gait][terrain][dn_drive] = ds
+                li.append(ds)
+    concat_datasets[subset] = ConcatDataset(li)
 
 
 # Train model
 if retrain_base:
     logger = TensorBoardLogger(base_dir / "logs", name="three_layer_mlp")
 
-    ds_all_gaits = ConcatDataset(list(datasets.values()))
-    train_ds, val_ds = random_split(ds_all_gaits, [0.8, 0.2])
-    train_loader = DataLoader(train_ds, batch_size=256, num_workers=4, shuffle=True)
-    val_loader = DataLoader(val_ds, batch_size=1028, num_workers=4)
+    train_ds, val_ds = random_split(concat_datasets["train"], [0.8, 0.2])
+    train_loader = DataLoader(train_ds, batch_size=256, num_workers=8, shuffle=True)
+    val_loader = DataLoader(val_ds, batch_size=1028, num_workers=8)
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",  # Name of your validation loss metric
+        dirpath=base_dir / "models",
+        filename="three_layer_mlp-{epoch:02d}-{val_loss:.2f}",
+        save_top_k=1,  # Save only the best checkpoint
+        mode="min",  # `min` for minimizing the validation loss
+    )
 
-    model = ThreeLayerMLP(input_size=42 + 6, hidden_size=8, output_size=2)
+    model = ThreeLayerMLP(input_size=42 + 6, hidden_size=32, output_size=2)
 
     trainer = pl.Trainer(
         logger=logger,
+        callbacks=[checkpoint_callback],
         max_epochs=max_epochs,
         check_val_every_n_epoch=1,
         deterministic=True,
@@ -70,166 +92,176 @@ if retrain_base:
     trainer.fit(model, train_loader, val_loader)
     torch.save(model.state_dict(), base_dir / "models/three_layer_mlp.pth")
 else:
-    model = ThreeLayerMLP(input_size=42 + 6, hidden_size=8, output_size=2)
+    model = ThreeLayerMLP(input_size=42 + 6, hidden_size=32, output_size=2)
     model.load_state_dict(torch.load(base_dir / "models/three_layer_mlp.pth"))
 
 
-# Make predictions
-predictions = {}
-performances = {}
-for gait, ds in datasets.items():
-    x_all = torch.tensor(np.concatenate([ds.joint_angles, ds.contact_mask], axis=1))
-    y_pred = model(x_all).detach().numpy()
-    predictions[gait] = y_pred
-    y_true = ds.roll_pitch_ts
-    r2_roll = r2_score(y_true[:, 0], y_pred[:, 0])
-    r2_pitch = r2_score(y_true[:, 1], y_pred[:, 1])
-    performances[gait] = {"roll": r2_roll, "pitch": r2_pitch}
-
-
-# Visualize predictions
-viz.make_sample_time_series_plot(
-    predictions=predictions,
-    performances=performances,
-    unmasked_datasets=datasets,
-    output_path=base_dir / "figs/three_layer_mlp.pdf",
-)
-
-
-# Select features based on importance
-# fmt: off
-dof_subsets = {
-    "All": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
-    "~(ThC pitch)": ["ThC_roll", "ThC_yaw", "CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
-    "~(ThC roll)": ["ThC_pitch", "ThC_yaw", "CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
-    "~(ThC yaw)": ["ThC_pitch", "ThC_roll", "CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
-    "~(CTr pitch)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
-    "~(CTr roll)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_pitch", "FTi_pitch", "TiTa_pitch"],
-    "~(FTi pitch)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_pitch", "CTr_roll", "TiTa_pitch"],
-    "~(TiTa pitch)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_pitch", "CTr_roll", "FTi_pitch"],
-    "~(ThC all)": ["CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
-    "~(CTr both)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "FTi_pitch", "TiTa_pitch"],
-    "ThC pitch": ["ThC_pitch"],
-    "ThC roll": ["ThC_roll"],
-    "ThC yaw": ["ThC_yaw"],
-    "CTr pitch": ["CTr_pitch"],
-    "CTr roll": ["CTr_roll"],
-    "FTi pitch": ["FTi_pitch"],
-    "TiTa pitch": ["TiTa_pitch"],
-    "ThC all": ["ThC_pitch", "ThC_roll", "ThC_yaw"],
-    "CTr both": ["CTr_pitch", "CTr_roll"],
-    "None": [],
-}
-# fmt: on
-
-
-def subset_to_mask(dof_subset):
-    _dof_name_lookup = {
-        "ThC_pitch": "Coxa",
-        "ThC_roll": "Coxa_roll",
-        "ThC_yaw": "Coxa_yaw",
-        "CTr_pitch": "Femur",
-        "CTr_roll": "Femur_roll",
-        "FTi_pitch": "Tibia",
-        "TiTa_pitch": "Tarsus1",
-    }
-    dof_subset = [_dof_name_lookup[dof] for dof in dof_subset]
-    mask = [dof in dof_subset for dof in flygym.preprogrammed.all_leg_dofs]
-    mask = []
-    for dof in flygym.preprogrammed.all_leg_dofs:
-        to_include = False
-        for dof_to_include in dof_subset:
-            if dof.endswith(dof_to_include):
-                to_include = True
-                break
-        mask.append(to_include)
-
-    return np.array(mask)
-
-
-# Feature selection
-models_all = {}
-predictions_all = {}
-performances_all = {}
-
-for dof_subset_tag, dofs in dof_subsets.items():
-    print(f"Processing {dof_subset_tag} model...")
-
-    # Load and mask out dataset
-    dof_mask = subset_to_mask(dofs)
-    masked_datasets = deepcopy(datasets)
-    for ds in masked_datasets.values():
-        ds.joint_angles[:, ~dof_mask] = 0
-
-    # Train model
-    if retrain_feature_selection:
-        ds_all_gaits = ConcatDataset(list(masked_datasets.values()))
-        train_ds, val_ds = random_split(ds_all_gaits, [0.8, 0.2])
-        train_loader = DataLoader(train_ds, batch_size=256, num_workers=4, shuffle=True)
-        val_loader = DataLoader(val_ds, batch_size=1028, num_workers=4)
-        model = ThreeLayerMLP(input_size=42 + 6, hidden_size=8, output_size=2)
-        logger = TensorBoardLogger(
-            base_dir / "logs", name="three_layer_mlp_ablation", version=dof_subset_tag
-        )
-        trainer = pl.Trainer(
-            logger=logger,
-            max_epochs=max_epochs,
-            check_val_every_n_epoch=1,
-            deterministic=True,
-        )
-        trainer.fit(model, train_loader, val_loader)
-        models_all[dof_subset_tag] = model
-        torch.save(
-            model.state_dict(),
-            base_dir / f"models/three_layer_mlp_{dof_subset_tag}.pth",
-        )
-        print(f"Done training {dof_subset_tag} model.")
-    else:
-        model = ThreeLayerMLP(input_size=42 + 6, hidden_size=8, output_size=2)
-        model.load_state_dict(
-            torch.load(base_dir / f"models/three_layer_mlp_{dof_subset_tag}.pth")
-        )
-        models_all[dof_subset_tag] = model
-
-    # Evaluate model
-    predictions_all[dof_subset_tag] = {}
-    performances_all[dof_subset_tag] = {}
-    for gait, ds in masked_datasets.items():
-        x_all = torch.tensor(np.concatenate([ds.joint_angles, ds.contact_mask], axis=1))
-        y_pred = model(x_all).detach().numpy()
-        predictions_all[dof_subset_tag][gait] = y_pred
-        y_true = ds.roll_pitch_ts
-        r2_roll = r2_score(y_true[:, 0], y_pred[:, 0])
-        r2_pitch = r2_score(y_true[:, 1], y_pred[:, 1])
-        performances_all[dof_subset_tag][gait] = {
-            "roll": r2_roll,
-            "pitch": r2_pitch,
-        }
-        print(f"  {gait} gait: r2_roll: {r2_roll:.3f}, r2_pitch: {r2_pitch:.3f}")
-
 # Visualize results
-for dof_subset_tag in tqdm(dof_subsets.keys(), desc="Visualizing results"):
-    viz.make_sample_time_series_plot(
-        predictions=predictions_all[dof_subset_tag],
-        performances=performances_all[dof_subset_tag],
-        unmasked_datasets=datasets,
-        output_path=base_dir / f"figs/three_layer_mlp_{dof_subset_tag}.pdf",
-        dof_subset_tag=dof_subset_tag,
-    )
-
-# Make bar plot for feature selection results
-lines = []
-for dof_subset_tag, perf_dict in performances_all.items():
-    for gait, scores in perf_dict.items():
-        for dof in ["roll", "pitch"]:
-            lines.append([dof_subset_tag, gait, dof, scores[dof]])
-performances_df = pd.DataFrame(lines)
-performances_df.columns = ["dof_subset", "gait", "dof", "r2_score"]
-performances_df = performances_df.set_index(["dof_subset", "gait", "dof"])
-
-
-viz.make_feature_selection_summary_plot(
-    performances_df=performances_df,
-    dof_subset_tags=list(dof_subsets.keys()),
-    output_path=base_dir / "figs/feature_ablation.pdf",
+viz.visualize_one_dataset(
+    model,
+    individual_datasets["test"],
+    output_path=base_dir / "figs/three_layer_mlp.pdf",
+    dn_drive="0.94_1.02",
 )
+
+
+# # Make predictions
+# predictions = {}
+# performances = {}
+# for gait, ds in datasets_xxx["test"].items():
+#     x_all = torch.tensor(np.concatenate([ds.joint_angles, ds.contact_mask], axis=1))
+#     y_pred = model(x_all).detach().numpy()
+#     predictions[gait] = y_pred
+#     y_true = ds.roll_pitch_ts
+#     r2_roll = r2_score(y_true[:, 0], y_pred[:, 0])
+#     r2_pitch = r2_score(y_true[:, 1], y_pred[:, 1])
+#     performances[gait] = {"roll": r2_roll, "pitch": r2_pitch}
+
+
+# # Visualize predictions
+# viz.make_sample_time_series_plot(
+#     predictions=predictions,
+#     performances=performances,
+#     unmasked_datasets=datasets_xxx["test"],
+#     output_path=base_dir / "figs/three_layer_mlp.pdf",
+# )
+
+
+# # Select features based on importance
+# # fmt: off
+# dof_subsets = {
+#     "All": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
+#     "~(ThC pitch)": ["ThC_roll", "ThC_yaw", "CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
+#     "~(ThC roll)": ["ThC_pitch", "ThC_yaw", "CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
+#     "~(ThC yaw)": ["ThC_pitch", "ThC_roll", "CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
+#     "~(CTr pitch)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
+#     "~(CTr roll)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_pitch", "FTi_pitch", "TiTa_pitch"],
+#     "~(FTi pitch)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_pitch", "CTr_roll", "TiTa_pitch"],
+#     "~(TiTa pitch)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "CTr_pitch", "CTr_roll", "FTi_pitch"],
+#     "~(ThC all)": ["CTr_pitch", "CTr_roll", "FTi_pitch", "TiTa_pitch"],
+#     "~(CTr both)": ["ThC_pitch", "ThC_roll", "ThC_yaw", "FTi_pitch", "TiTa_pitch"],
+#     "ThC pitch": ["ThC_pitch"],
+#     "ThC roll": ["ThC_roll"],
+#     "ThC yaw": ["ThC_yaw"],
+#     "CTr pitch": ["CTr_pitch"],
+#     "CTr roll": ["CTr_roll"],
+#     "FTi pitch": ["FTi_pitch"],
+#     "TiTa pitch": ["TiTa_pitch"],
+#     "ThC all": ["ThC_pitch", "ThC_roll", "ThC_yaw"],
+#     "CTr both": ["CTr_pitch", "CTr_roll"],
+#     "None": [],
+# }
+# # fmt: on
+
+
+# def subset_to_mask(dof_subset):
+#     _dof_name_lookup = {
+#         "ThC_pitch": "Coxa",
+#         "ThC_roll": "Coxa_roll",
+#         "ThC_yaw": "Coxa_yaw",
+#         "CTr_pitch": "Femur",
+#         "CTr_roll": "Femur_roll",
+#         "FTi_pitch": "Tibia",
+#         "TiTa_pitch": "Tarsus1",
+#     }
+#     dof_subset = [_dof_name_lookup[dof] for dof in dof_subset]
+#     mask = [dof in dof_subset for dof in flygym.preprogrammed.all_leg_dofs]
+#     mask = []
+#     for dof in flygym.preprogrammed.all_leg_dofs:
+#         to_include = False
+#         for dof_to_include in dof_subset:
+#             if dof.endswith(dof_to_include):
+#                 to_include = True
+#                 break
+#         mask.append(to_include)
+
+#     return np.array(mask)
+
+
+# # Feature selection
+# pl.pytorch.seed_everything(0, workers=True)
+# models_all = {}
+# predictions_all = {}
+# performances_all = {}
+
+# for dof_subset_tag, dofs in dof_subsets.items():
+#     print(f"Processing {dof_subset_tag} model...")
+
+#     # Load and mask out dataset
+#     dof_mask = subset_to_mask(dofs)
+#     masked_datasets = deepcopy(datasets_xxx["train"])
+#     for ds in masked_datasets.values():
+#         ds.joint_angles[:, ~dof_mask] = 0
+
+#     # Train model
+#     if retrain_feature_selection:
+#         ds_all_gaits = ConcatDataset(list(masked_datasets.values()))
+#         train_ds, val_ds = random_split(ds_all_gaits, [0.8, 0.2])
+#         train_loader = DataLoader(train_ds, batch_size=256, num_workers=4, shuffle=True)
+#         val_loader = DataLoader(val_ds, batch_size=1028, num_workers=4)
+#         model = ThreeLayerMLP(input_size=42 + 6, hidden_size=8, output_size=2)
+#         logger = TensorBoardLogger(
+#             base_dir / "logs", name="three_layer_mlp_ablation", version=dof_subset_tag
+#         )
+#         trainer = pl.Trainer(
+#             logger=logger,
+#             max_epochs=max_epochs,
+#             check_val_every_n_epoch=1,
+#             deterministic=True,
+#         )
+#         trainer.fit(model, train_loader, val_loader)
+#         models_all[dof_subset_tag] = model
+#         torch.save(
+#             model.state_dict(),
+#             base_dir / f"models/three_layer_mlp_{dof_subset_tag}.pth",
+#         )
+#         print(f"Done training {dof_subset_tag} model.")
+#     else:
+#         model = ThreeLayerMLP(input_size=42 + 6, hidden_size=8, output_size=2)
+#         model.load_state_dict(
+#             torch.load(base_dir / f"models/three_layer_mlp_{dof_subset_tag}.pth")
+#         )
+#         models_all[dof_subset_tag] = model
+
+#     # Evaluate model
+#     predictions_all[dof_subset_tag] = {}
+#     performances_all[dof_subset_tag] = {}
+#     for gait, ds in masked_datasets.items():
+#         x_all = torch.tensor(np.concatenate([ds.joint_angles, ds.contact_mask], axis=1))
+#         y_pred = model(x_all).detach().numpy()
+#         predictions_all[dof_subset_tag][gait] = y_pred
+#         y_true = ds.roll_pitch_ts
+#         r2_roll = r2_score(y_true[:, 0], y_pred[:, 0])
+#         r2_pitch = r2_score(y_true[:, 1], y_pred[:, 1])
+#         performances_all[dof_subset_tag][gait] = {
+#             "roll": r2_roll,
+#             "pitch": r2_pitch,
+#         }
+#         print(f"  {gait} gait: r2_roll: {r2_roll:.3f}, r2_pitch: {r2_pitch:.3f}")
+
+# # Visualize results
+# for dof_subset_tag in tqdm(dof_subsets.keys(), desc="Visualizing results"):
+#     viz.make_sample_time_series_plot(
+#         predictions=predictions_all[dof_subset_tag],
+#         performances=performances_all[dof_subset_tag],
+#         unmasked_datasets=datasets_xxx["train"],
+#         output_path=base_dir / f"figs/three_layer_mlp_{dof_subset_tag}.pdf",
+#         dof_subset_tag=dof_subset_tag,
+#     )
+
+# # Make bar plot for feature selection results
+# lines = []
+# for dof_subset_tag, perf_dict in performances_all.items():
+#     for gait, scores in perf_dict.items():
+#         for dof in ["roll", "pitch"]:
+#             lines.append([dof_subset_tag, gait, dof, scores[dof]])
+# performances_df = pd.DataFrame(lines)
+# performances_df.columns = ["dof_subset", "gait", "dof", "r2_score"]
+# performances_df = performances_df.set_index(["dof_subset", "gait", "dof"])
+
+
+# viz.make_feature_selection_summary_plot(
+#     performances_df=performances_df,
+#     dof_subset_tags=list(dof_subsets.keys()),
+#     output_path=base_dir / "figs/feature_ablation.pdf",
+# )
--- flygym/examples/head_stabilization/viz.py
+++ flygym/examples/head_stabilization/viz.py
@@ -1,58 +1,96 @@
 import numpy as np
+import torch
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 from pathlib import Path
-from typing import Dict, List, Optional
-from torch.utils.data import Dataset
+from typing import Dict, List, Optional, Callable
 from pandas import DataFrame
+from sklearn.metrics import r2_score
+from flygym.examples.head_stabilization import WalkingDataset
 
 
 plt.rcParams["font.family"] = "Arial"
 plt.rcParams["pdf.fonttype"] = 42
 
 
-def make_sample_time_series_plot(
-    predictions: Dict[str, np.ndarray],
-    performances: Dict[str, Dict[str, float]],
-    unmasked_datasets: Dict[str, Dataset],
+_color_config = {
+    "roll": ("royalblue", "midnightblue"),
+    "pitch": ("peru", "saddlebrown"),
+}
+_marker_config = {
+    "tripod": "^",
+    "tetrapod": "s",
+    "wave": "d",
+}
+
+
+def visualize_one_dataset(
+    model: Callable,
+    test_datasets: Dict[str, Dict[str, Dict[str, WalkingDataset]]],
     output_path: Path,
-    dof_subset_tag: Optional[str] = None,
+    joint_angles_mask: Optional[np.ndarray] = None,
+    subset_tag: Optional[str] = None,
+    dn_drive: str = "0.94_1.02",
 ):
     fig, axs = plt.subplots(
-        3, 2, figsize=(8, 6), tight_layout=True, sharex=True, sharey=True
+        3, 2, figsize=(9, 6), tight_layout=True, sharex=True, sharey=True
     )
-    for i, (gait, ds) in enumerate(unmasked_datasets.items()):
-        t_grid = (np.arange(len(ds)) + ds.ignore_first_n) * 1e-4
-        for j, dof in enumerate(["roll", "pitch"]):
-            axs[i, j].plot(
-                t_grid,
-                np.rad2deg(unmasked_datasets[gait].roll_pitch_ts[:, j]),
-                lw=1,
-                color="black",
-                label="Actual",
-            )
-            axs[i, j].plot(
-                t_grid,
-                np.rad2deg(predictions[gait][:, j]),
-                lw=1,
-                color="tab:red",
-                label="Predicted",
-                linestyle="--",
-            )
-            axs[i, j].text(
-                1.0,
-                0.01,
-                f"$R^2$={performances[gait][dof]:.2f}",
-                ha="right",
-                va="bottom",
-                transform=axs[i, j].transAxes,
-            )
-            axs[i, j].set_ylim(-15, 15)
-            axs[i, j].set_xlim(51, 52)
+    for i, gait in enumerate(["tripod", "tetrapod", "wave"]):
+        for j, terrain in enumerate(["flat", "blocks"]):
+            # Collect data
+            ds = test_datasets[gait][terrain][dn_drive]
+            joint_angles = ds.joint_angles
+            if joint_angles_mask is not None:
+                joint_angles = joint_angles.copy()
+                joint_angles = joint_angles[:, joint_angles_mask]
+            contact_mask = ds.contact_mask
+            y_true = ds.roll_pitch_ts
+
+            # Make predictions
+            x = np.concatenate([joint_angles, contact_mask], axis=1)
+            y_pred = model(torch.tensor(x[None, ...])).detach().cpu().numpy().squeeze()
+
+            # Evaluate performance
+            perf = {}
+            for k, dof in enumerate(["roll", "pitch"]):
+                perf[dof] = r2_score(y_true[:, k], y_pred[:, k])
+
+            # Visualize
+            ax = axs[i, j]
+            t_grid = (np.arange(len(ds)) + ds.ignore_first_n) * 1e-4
+            for k, dof in enumerate(["roll", "pitch"]):
+                color_light, color_dark = _color_config[dof]
+                ax.plot(
+                    t_grid,
+                    np.rad2deg(y_true[:, k]),
+                    linestyle="--",
+                    lw=1,
+                    color=color_light,
+                    label=f"Actual {dof}",
+                )
+                ax.plot(
+                    t_grid,
+                    np.rad2deg(y_pred[:, k]),
+                    linestyle="-",
+                    lw=1,
+                    color=color_dark,
+                    label=f"Predicted {dof}",
+                )
+                axs[i, j].text(
+                    1.0,
+                    0.01 if k == 0 else 0.1,
+                    f"{dof.title()}: $R^2$={perf[dof]:.2f}",
+                    ha="right",
+                    va="bottom",
+                    transform=axs[i, j].transAxes,
+                    color=color_dark,
+                )
+            if i == 0 and j == 1:
+                ax.legend(frameon=False, bbox_to_anchor=(1.04, 1), loc="upper left")
             if i == 0:
-                axs[i, j].set_title(rf"Thorax {dof}", size=12)
+                axs[i, j].set_title(rf"{terrain.title()} terrain", size=12)
             if j == 0:
                 axs[i, j].text(
                     -0.3,
@@ -67,11 +105,11 @@ def make_sample_time_series_plot(
                 axs[i, j].set_xlabel("Time [s]")
             if j == 0:
                 axs[i, j].set_ylabel(r"Angle [$^\circ$]")
-            if i == 0 and j == 0:
-                axs[i, j].legend(frameon=False)
-            sns.despine(ax=axs[i, j])
-    if dof_subset_tag is not None:
-        fig.suptitle(f"DoF selection: {dof_subset_tag}", fontweight="bold")
+            ax.set_xlim(0.5, 1.5)
+            ax.set_ylim(-45, 45)
+            sns.despine(ax=ax)
+    if subset_tag is not None:
+        fig.suptitle(f"DoF selection: {subset_tag}", fontweight="bold")
     fig.savefig(output_path)
     plt.close(fig)
 
@@ -82,17 +120,8 @@ def make_feature_selection_summary_plot(
     fig, ax = plt.subplots(figsize=(6, 3), tight_layout=True)
     bar_width = 0.4
     category_base = np.arange(len(dof_subset_tags))
-    color_config = {
-        "roll": ("royalblue", "midnightblue"),
-        "pitch": ("peru", "saddlebrown"),
-    }
-    marker_config = {
-        "tripod": "^",
-        "tetrapod": "s",
-        "wave": "d",
-    }
     for dof in ["roll", "pitch"]:
-        color_light, color_dark = color_config[dof]
+        color_light, color_dark = _color_config[dof]
         xs = category_base + (0.5 * bar_width * (-1 if dof == "roll" else 1))
         mean_r2s = [
             performances_df.loc[cat, :, dof]["r2_score"].mean()
@@ -104,7 +133,7 @@ def make_feature_selection_summary_plot(
                 performances_df.loc[cat, gait, dof]["r2_score"]
                 for cat in dof_subset_tags
             ]
-            ax.scatter(xs, ys, color=color_dark, marker=marker_config[gait], s=5)
+            ax.scatter(xs, ys, color=color_dark, marker=_marker_config[gait], s=5)
     ax.set_ylabel("$R^2$")
     ax.set_xlabel("")
     ax.tick_params(axis="x", labelrotation=90)
