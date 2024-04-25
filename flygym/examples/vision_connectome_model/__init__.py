106a3462fbf883f33b8960f18fc88b1bb2419499
Sibo Wang <sibo.wang@epfl.ch>
2024-04-25 19:18:48 +0200

    add fly following trajectory and example visual neuron activities figure

--- flygym/examples/vision_connectome_model/__init__.py
+++ flygym/examples/vision_connectome_model/__init__.py
@@ -1,4 +1,3 @@
 from flygym.examples.vision_connectome_model.arena import *
 from flygym.examples.vision_connectome_model.network import *
-from flygym.examples.vision_connectome_model.viz import *
 from flygym.examples.vision_connectome_model.controller import *
