222ea5029c84c7fe498ead88658428ca7766bea2
Sibo Wang <sibo.wang@epfl.ch>
2024-05-16 17:55:24 +0200

    add torch to dependency list for automated testing

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
             "phiflow",
             "flyvision @ https://github.com/TuragaLab/flyvis/archive/refs/heads/main.zip",
         ],
-        "tests": ["networkx", "h5py", "scikit-learn"],
+        "tests": ["networkx", "h5py", "scikit-learn", "torch"],
     },
     url="https://neuromechfly.org/",
     long_description=open("README.md", encoding="UTF-8").read(),
