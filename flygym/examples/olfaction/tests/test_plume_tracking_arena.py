1c3e1d335dba5472e62151a46c467fb8bdbecc4f
Sibo Wang <sibo.wang@epfl.ch>
2024-05-27 11:28:42 +0200

    add plume tracking test

---
+++ flygym/examples/olfaction/tests/test_plume_tracking_arena.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pytest
+from flygym.examples.olfaction import OdorPlumeArena
+from flygym.util import get_data_path
+
+
+def test_plume_arena():
+    plume_data_path = get_data_path(
+        "flygym", "data/test_data/plume_tracking/plume_short.hdf5"
+    )
+    arena = OdorPlumeArena(
+        plume_data_path=plume_data_path,
+        plume_simulation_fps=20,
+        intensity_scale_factor=1.0,
+    )
+    positions_1 = np.array([[0, 80, 1], [10, 80, 1], [52, 80, 1], [75, 105, 1]])
+    positions_2 = np.array([[75, 15, 1], [10, 80, 1], [239, 159, 1], [240, 160, 1]])
+    intensities_1 = arena.get_olfaction(positions_1)
+    intensities_2 = arena.get_olfaction(positions_2)
+    assert intensities_1[0, :] == pytest.approx([0, 0.45141602, 0.58837891, 0.17651369])
+    assert intensities_2[0, :3] == pytest.approx([0, 0.45141602, 0])
+    assert np.isnan(intensities_2[0, 3])  # out of bound
---
+++ flygym/examples/olfaction/tests/test_plume_tracking_task.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pytest
+from flygym import Fly, Camera
+from flygym.examples.olfaction import OdorPlumeArena, PlumeNavigationTask
+from flygym.util import get_data_path
+
+
+def test_plume_tracking_task():
+    plume_data_path = get_data_path(
+        "flygym", "data/test_data/plume_tracking/plume_short.hdf5"
+    )
+    arena = OdorPlumeArena(
+        plume_data_path=plume_data_path,
+        plume_simulation_fps=20,
+        intensity_scale_factor=1.0,
+    )
+    contact_sensor_placements = [
+        f"{leg}{segment}"
+        for leg in ["LF", "LM", "LH", "RF", "RM", "RH"]
+        for segment in ["Tibia", "Tarsus1", "Tarsus2", "Tarsus3", "Tarsus4", "Tarsus5"]
+    ]
+    fly = Fly(
+        enable_adhesion=True,
+        draw_adhesion=True,
+        enable_olfaction=True,
+        enable_vision=False,
+        contact_sensor_placements=contact_sensor_placements,
+        spawn_pos=(40, 80, 0.25),
+        spawn_orientation=(0, 0, -np.pi / 2),
+    )
+    cam = Camera(fly=fly, camera_id="birdeye_cam", play_speed=0.5, timestamp_text=True)
+    sim = PlumeNavigationTask(
+        fly=fly,
+        arena=arena,
+        cameras=[cam],
+    )
+    sim_time = 0.1
+    rendered_images = []
+    obs_hist = []
+    info_hist = []
+    for i in range(int(sim_time / sim.timestep)):
+        obs, _, _, _, info = sim.step(np.array([1, 1]))
+        img = sim.render()[0]
+        obs_hist.append(obs)
+        info_hist.append(info)
+        if img is not None:
+            rendered_images.append(img)
+
+    # Check format of observation and info
+    expected_obs_keys = {
+        "joints",
+        "fly",
+        "contact_forces",
+        "end_effectors",
+        "fly_orientation",
+        "odor_intensity",
+    }
+    expected_info_keys = {"net_corrections", "joints", "adhesion"}
+    for obs in obs_hist:
+        assert set(obs.keys()) == expected_obs_keys
+    for info in info_hist:
+        assert set(info.keys()) == expected_info_keys
+
+    odor_hist = np.array([obs["odor_intensity"] for obs in obs_hist])
+    assert odor_hist.shape == (int(sim_time / sim.timestep), 1, 4)
+    assert np.unique(odor_hist[:, 0, 0]).size >= 2
+
+    # Check position mapping for rendering
+    pos_display_sample, pos_physical_sample = arena.get_position_mapping(
+        sim, camera_id="birdeye_cam"
+    )
+    assert pos_display_sample.shape == (160, 240, 2)
+    assert pos_physical_sample.shape == (160, 240, 2)
+    # plt.imshow(rendered_images[0])
+    # plt.show()  # check if this makes sense. if so, the following is golden data
+    # print(pos_display_sample.sum(), pos_physical_sample.sum())
+    # print(sim.grid_idx_all.sum(), sim.grid_idx_all[90, 100])
+    assert pos_display_sample.sum() == pytest.approx(20562741.464867506, rel=1e-6)
+    assert pos_physical_sample.sum() == pytest.approx(7680000.0, rel=1e-6)
+    assert sim.grid_idx_all.sum() == 84662700
+    assert np.all(sim.grid_idx_all[90, 100] == [46, 270])
+    assert not np.all(rendered_images[0] == rendered_images[-1])
