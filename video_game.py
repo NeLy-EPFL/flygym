ac499f2bd2d93a47f8d71c43feebe4026f717517
NathanMULLER <muller.nathan1867@gmail.com>
2024-11-06 23:12:28 +0100

    game working with joystick AND keyboard

--- video_game.py
+++ video_game.py
@@ -36,7 +36,7 @@ contact_sensor_placements = [
 ]
 
 
-# Initial configuration
+# Configuration initiale
 fly = Fly(init_pose="stretch", actuated_joints=actuated_joints, contact_sensor_placements=contact_sensor_placements, control="position", self_collisions="none", xml_variant="seqik", joint_damping=0.3, joint_stiffness=0.01)
 cam = Camera(fly=fly, play_speed=0.1, draw_contacts=False, camera_id="Animat/camera_top", window_size=(1280, 720), camera_follows_fly_orientation=True, fps=30)
 sim = HybridTurningController(
@@ -46,8 +46,8 @@ sim = HybridTurningController(
     timestep=timestep,
 )
 obs, info = sim.reset()
-gain_left = 1
-gain_right = 1
+gain_left = 0
+gain_right = 0
 action = np.array([gain_right, gain_left])
 activated_legs = np.ones(6)
 full_control = True
@@ -83,6 +83,7 @@ x_position_0 = x_start
 y_position_0 = y_start
 count_speed = 0
 
+joystick_connected = False
 
 
 while(not quit):
@@ -99,26 +100,62 @@ while(not quit):
                 joy = pygame.joystick.Joystick(event.device_index)
                 joysticks[joy.get_instance_id()] = joy
                 print(f"Joystick {joy.get_instance_id()} connected")
+                joystick_connected = True
+                timer_LF = 0
+                timer_LF_activated = False
+                timer_LM = 0
+                timer_LM_activated = False
+                timer_LH = 0
+                timer_LH_activated = False
+                timer_RF = 0
+                timer_RF_activated = False
+                timer_RM = 0
+                timer_RM_activated = False
+                timer_RH = 0
+                timer_RH_activated = False
+                timer_L = 0
+                timer_L_activated = False
+                timer_R = 0
+                timer_R_activated = False
 
             if event.type == pygame.JOYDEVICEREMOVED:
                 del joysticks[event.instance_id]
                 print(f"Joystick {event.instance_id} disconnected")
+                joystick_connected = False
+                timer_LF = 0
+                timer_LF_activated = False
+                timer_LM = 0
+                timer_LM_activated = False
+                timer_LH = 0
+                timer_LH_activated = False
+                timer_RF = 0
+                timer_RF_activated = False
+                timer_RM = 0
+                timer_RM_activated = False
+                timer_RH = 0
+                timer_RH_activated = False
+                timer_L = 0
+                timer_L_activated = False
+                timer_R = 0
+                timer_R_activated = False
 
         # For each joystick:
-        for joystick in joysticks.values():
+        if joystick_connected:
+            for joystick in joysticks.values():
 
-            axes = joystick.get_numaxes()
+                axes = joystick.get_numaxes()
 
-            axis = np.zeros(axes)
+                axis = np.zeros(axes)
 
-            for i in range(axes):
-                axis[i] = joystick.get_axis(i)
+                for i in range(axes):
+                    axis[i] = joystick.get_axis(i)
 
-            buttons = joystick.get_numbuttons()
-            button = np.zeros(buttons)
+                buttons = joystick.get_numbuttons()
+                button = np.zeros(buttons)
 
-            for i in range(buttons):
-                button[i] = joystick.get_button(i)
+                for i in range(buttons):
+                    button[i] = joystick.get_button(i)
+
 
         x_position_1 = obs['fly'][0][0]
         y_position_1 = obs['fly'][0][1]
@@ -213,66 +250,268 @@ while(not quit):
                 activated_legs[5] = 0
         
 
-        # Update with new gains
+        # Met à jour avec les nouveaux gains
         action = np.array([gain_right, gain_left])
 
         obs, reward, terminated, truncated, info = sim.step(action, activated_legs)
         
-        # Render the simulation
-        image = sim.render(camera_name='cam', width=1000, height=1000) 
+        # Rendu de l'image
+        image = sim.render(camera_name='cam', width=1000, height=1000)  # Rendu via la caméra
         
-        # Check if the image is valid
+        # Vérifie si l'image est correcte
         if not isinstance(image, list) or len(image) == 0 or image[0] is None:
-            continue  # Ignore this iteration
+            continue  # Ignore cette itération si aucune image n'est retournée
 
-        image = image[0]  # Take the first image 
+        image = image[0]  # Prend le premier élément de la liste
         
-        # Make sure the image is a NumPy array of the correct type
+        # Assure que c'est un tableau NumPy
         if isinstance(image, np.ndarray):
-            if image.dtype != np.uint8:  # Convert to uint8 if not
+            if image.dtype != np.uint8:  # Convertit au format uint8 si pas le cas
                 image = image.astype(np.uint8)
         else:
-            continue  # If not a NumPy array, ignore this iteration
+            continue  # Ignore si ce n'est pas un tableau NumPy
         
-        image = np.squeeze(image)  # Delete singleton dimensions
+        image = np.squeeze(image)  # Supprime les dimensions de taille 1
         im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         im_txt = cv2.putText(im_rgb, state, org, font, fontScale, color, thickness, cv2.LINE_AA)
         im_distance = cv2.putText(im_txt, f'Distance : {distance} mm', (50, 100), font, fontScale, color, thickness, cv2.LINE_AA)
         im_speed = cv2.putText(im_distance, f'speed : {speed:.1f} mm/s', (50, 150), font, fontScale, color, thickness, cv2.LINE_AA)
         
-        # Update the display
+        # Mise à jour de l'image affichée
         cv2.imshow('Simulation', im_speed)
 
-        if button[0] == 0:
-            waiting = True
+        if joystick_connected:
+            if button[0] == 0:
+                waiting = True
 
-        if waiting:
-            if button[0] == 1:
-                waiting = False
-                if full_control:
-                    full_control = False
-                    phase_control = True
-                    state = 'Tripod'
-                    single_control = False
-                    activated_legs = np.zeros(6)
-                    action = np.array([1, 1])
-                        
+            if waiting:
+                if button[0] == 1:
+                    waiting = False
+                    if full_control:
+                        full_control = False
+                        phase_control = True
+                        state = 'Tripod'
+                        single_control = False
+                        activated_legs = np.zeros(6)
+                        action = np.array([1, 1])
+                            
 
+                    elif phase_control:
+                        phase_control = False
+                        full_control = False
+                        state = 'Single Leg Control'
+                        single_control = True
+                        activated_legs = np.zeros(6)
+                        action = np.array([1, 1])
+
+                    elif single_control:
+                        single_control = False
+                        phase_control = False
+                        full_control = True
+                        state = 'CPG Control'
+                        activated_legs = np.ones(6)
+
+                    timer_LF = 0
+                    timer_LF_activated = False
+                    timer_LM = 0
+                    timer_LM_activated = False
+                    timer_LH = 0
+                    timer_LH_activated = False
+                    timer_RF = 0
+                    timer_RF_activated = False
+                    timer_RM = 0
+                    timer_RM_activated = False
+                    timer_RH = 0
+                    timer_RH_activated = False
+                    timer_L = 0
+                    timer_L_activated = False
+                    timer_R = 0
+                    timer_R_activated = False
+
+            if button[1] == 1:
+                quit = True
+                break
+
+
+            if full_control:
+                if axis[0] > 0.2 and (axis[1] > 0.2 or axis[1] < -0.2):
+                    gain_left = -axis[1] * (1- axis[0])
+                    gain_right = -axis[1] * axis[0]
+                elif axis[0] < -0.2 and (axis[1] > 0.2 or axis[1] < -0.2):
+                    gain_left = -axis[1] * -axis[0]
+                    gain_right = -axis[1] * (1 + axis[0])
+                elif axis[0] > 0.2 and (axis[1] > -0.2 or axis[1] < 0.2):
+                    gain_left = axis[0]
+                    gain_right = 0
+                elif axis[0] < -0.2 and (axis[1] > -0.2 or axis[1] < 0.2):
+                    gain_left = 0
+                    gain_right = -axis[0]
+                elif axis[1] < 0.1 and axis[1] > -0.1 and axis[0] < 0.1 and axis[0] > -0.1:
+                    gain_left = 0
+                    gain_right = 0
+                else:
+                    gain_left = -axis[1]
+                    gain_right = -axis[1]
+
+            if phase_control:
+                gain_left = 1
+                gain_right = 1
+                if not timer_L_activated:
+                    if button[2] == 1:
+                        timer_L_activated = True
+                        activated_legs[0] = 1 
+                        activated_legs[2] = 1 
+                        activated_legs[4] = 1
+                if not timer_R_activated:
+                    if button[3] == 1:
+                        timer_R_activated = True
+                        activated_legs[1] = 1
+                        activated_legs[3] = 1
+                        activated_legs[5] = 1
+
+            if single_control:
+                gain_left = 1
+                gain_right = 1
+                if not timer_LF_activated:
+                    if button[10] == 1:
+                        timer_LF_activated = True
+                        activated_legs[0] = 1
+                if not timer_LM_activated:
+                    if button[11] == 1:
+                        timer_LM_activated = True
+                        activated_legs[1] = 1
+                if not timer_LH_activated:
+                    if button[12] == 1:
+                        timer_LH_activated = True
+                        activated_legs[2] = 1
+                if not timer_RF_activated:
+                    if button[4] == 1:
+                        timer_RF_activated = True
+                        activated_legs[3] = 1
+                if not timer_RM_activated:
+                    if button[5] == 1:
+                        timer_RM_activated = True
+                        activated_legs[4] = 1
+                if not timer_RH_activated:
+                    if button[6] == 1:
+                        timer_RH_activated = True
+                        activated_legs[5] = 1
+        else:
+            
+            # Vérifie si une touche est appuyée
+            key = cv2.waitKey(1) & 0xFF
+
+            # Si 'Q' est pressé, augmente le gain gauche et diminue le gain droit (tourner à gauche)
+            if key == ord('q'):
+                gain_left = 1 
+                gain_right = -1
+
+            # Si 'D' est pressé, augmente le gain droit et diminue le gain gauche (tourner à droite)
+            if key == ord('d'):
+                gain_right = 1 
+                gain_left = -1
+
+            # Si 'Z' est pressé, augmente le gain droit et le gain gauche (tout droit)
+            if key == ord('z'):
+                gain_right = 1 
+                gain_left = 1
+            
+            # Si 'S' est pressé, diminue le gain droit et le gain gauche (recule)
+            if key == ord('s'):
+                gain_right = -1 
+                gain_left = -1
+
+            # Quitte si la touche 'e' est pressée
+            if key == ord('e'):
+                quit = True
+                break
+            
+            # Si 'A' est pressé, met le gain droit et le gain gauche à 0 (stop)
+            if key == ord('a'):
+                gain_right = 0 
+                gain_left = 0
+                
+            if key == ord('t'):
+                if single_control:
+                    if not timer_LF_activated:
+                        timer_LF_activated = True
+                        activated_legs[0] = 1
+                    
+
+            if key == ord('g'):
+                if single_control:
+                    if not timer_LM_activated:
+                        timer_LM_activated = True
+                        activated_legs[1] = 1
                 elif phase_control:
-                    phase_control = False
-                    full_control = False
-                    state = 'Single Leg Control'
-                    single_control = True
-                    activated_legs = np.zeros(6)
-                    action = np.array([1, 1])
+                    if not timer_L_activated:
+                        timer_L_activated = True
+                        activated_legs[0] = 1 
+                        activated_legs[2] = 1 
+                        activated_legs[4] = 1
+            
+            if key == ord('b'):
+                if single_control:
+                    if not timer_LH_activated :
+                        timer_LH_activated = True
+                        activated_legs[2] = 1
+                    
 
-                elif single_control:
-                    single_control = False
-                    phase_control = False
-                    full_control = True
-                    state = 'CPG Control'
-                    activated_legs = np.ones(6)
+            if key == ord('y'):
+                if single_control:
+                    if not timer_RF_activated:
+                        timer_RF_activated = True
+                        activated_legs[3] = 1
 
+            if key == ord('h'):
+                if single_control:
+                    if not timer_RM_activated:
+                        timer_RM_activated = True
+                        activated_legs[4] = 1 
+                elif phase_control:
+                    if not timer_R_activated:
+                        timer_R_activated = True
+                        activated_legs[1] = 1
+                        activated_legs[3] = 1
+                        activated_legs[5] = 1
+
+            if key == ord('n'):
+                if single_control:
+                    if not timer_RH_activated:
+                        timer_RH_activated = True
+                        activated_legs[5] = 1
+            
+            if key == ord('p'): #tripod control
+                phase_control = True
+                state = 'Tripod'
+                activated_legs = np.zeros(6)
+                action = np.array([1, 1])
+                full_control = False
+                single_control = False
+                timer_LF = 0
+                timer_LF_activated = False
+                timer_LM = 0
+                timer_LM_activated = False
+                timer_LH = 0
+                timer_LH_activated = False
+                timer_RF = 0
+                timer_RF_activated = False
+                timer_RM = 0
+                timer_RM_activated = False
+                timer_RH = 0
+                timer_RH_activated = False
+                timer_L = 0
+                timer_L_activated = False
+                timer_R = 0
+                timer_R_activated = False
+            
+            if key == ord('o'): #single leg control
+                single_control = True
+                state = 'Single Leg Control'
+                activated_legs = np.zeros(6)
+                action = np.array([1, 1])
+                phase_control = False
+                full_control = False
                 timer_LF = 0
                 timer_LF_activated = False
                 timer_LM = 0
@@ -290,78 +529,31 @@ while(not quit):
                 timer_R = 0
                 timer_R_activated = False
 
-        if button[1] == 1:
-            quit = True
-            break
-
-
-        if full_control:
-            if axis[0] > 0.2 and (axis[1] > 0.2 or axis[1] < -0.2):
-                gain_left = -axis[1] * (1- axis[0])
-                gain_right = -axis[1] * axis[0]
-            elif axis[0] < -0.2 and (axis[1] > 0.2 or axis[1] < -0.2):
-                gain_left = -axis[1] * -axis[0]
-                gain_right = -axis[1] * (1 + axis[0])
-            elif axis[0] > 0.2 and (axis[1] > -0.2 or axis[1] < 0.2):
-                gain_left = axis[0]
-                gain_right = 0
-            elif axis[0] < -0.2 and (axis[1] > -0.2 or axis[1] < 0.2):
-                gain_left = 0
-                gain_right = -axis[0]
-            elif axis[1] < 0.1 and axis[1] > -0.1 and axis[0] < 0.1 and axis[0] > -0.1:
-                gain_left = 0
-                gain_right = 0
-            else:
-                gain_left = -axis[1]
-                gain_right = -axis[1]
-
-        if phase_control:
-            gain_left = 1
-            gain_right = 1
-            if not timer_L_activated:
-                if button[2] == 1:
-                    timer_L_activated = True
-                    activated_legs[0] = 1 
-                    activated_legs[2] = 1 
-                    activated_legs[4] = 1
-            if not timer_R_activated:
-                if button[3] == 1:
-                    timer_R_activated = True
-                    activated_legs[1] = 1
-                    activated_legs[3] = 1
-                    activated_legs[5] = 1
-
-        if single_control:
-            gain_left = 1
-            gain_right = 1
-            if not timer_LF_activated:
-                if button[10] == 1:
-                    timer_LF_activated = True
-                    activated_legs[0] = 1
-            if not timer_LM_activated:
-                if button[11] == 1:
-                    timer_LM_activated = True
-                    activated_legs[1] = 1
-            if not timer_LH_activated:
-                if button[12] == 1:
-                    timer_LH_activated = True
-                    activated_legs[2] = 1
-            if not timer_RF_activated:
-                if button[4] == 1:
-                    timer_RF_activated = True
-                    activated_legs[3] = 1
-            if not timer_RM_activated:
-                if button[5] == 1:
-                    timer_RM_activated = True
-                    activated_legs[4] = 1
-            if not timer_RH_activated:
-                if button[6] == 1:
-                    timer_RH_activated = True
-                    activated_legs[5] = 1
+            if key == ord('i'): #full control
+                full_control = True
+                state = 'CPG Control'
+                activated_legs = np.ones(6)
+                phase_control = False
+                single_control = False
+                timer_LF = 0
+                timer_LF_activated = False
+                timer_LM = 0
+                timer_LM_activated = False
+                timer_LH = 0
+                timer_LH_activated = False
+                timer_RF = 0
+                timer_RF_activated = False
+                timer_RM = 0
+                timer_RM_activated = False
+                timer_RH = 0
+                timer_RH_activated = False
+                timer_L = 0
+                timer_L_activated = False
+                timer_R = 0
+                timer_R_activated = False
         
 
         
 
-# Close the window and quit.
+# Ferme toutes les fenêtres OpenCV une fois la simulation terminée
 cv2.destroyAllWindows()
-
