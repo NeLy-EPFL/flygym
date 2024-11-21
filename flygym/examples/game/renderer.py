ef35946dca3e1e29a2816f3100d7e2162492411a
stimpfli <victor.stimpfling@epfl.ch>
2024-11-21 15:43:54 +0100

    add energy consumption

--- flygym/examples/game/renderer.py
+++ flygym/examples/game/renderer.py
@@ -21,6 +21,15 @@ class Renderer:
         }
 
         self.leaderboard_boundaries = [(1280-400, 720 - 230), (1280, 720)]
+        
+        self.consumed_energy_ys = [50, 80]
+        self.energy_bar_width = np.rint(0.4*self.window_size[0]).astype(int) # 40% of the window width
+        self.energy_mutlipler = 1e6
+        self.energy_unit = "micro Joules"
+        self.energy_bar_min = 0
+        self.energy_bar_max = 10
+        self.energy_bar_graduations = 10
+        self.energy_bar_graduation_increment = self.energy_bar_max // self.energy_bar_graduations
 
     def initialize_window(self):
         cv2.namedWindow(self.window_name, cv2.WND_PROP_FULLSCREEN)
@@ -32,7 +41,7 @@ class Renderer:
     def update_speed(self, speed, i):
         self.speed_list[i % self.speed_window_size] = speed
 
-    def prepare_simple_image(self, image, state, time):
+    def prepare_simple_image(self, image, state, time, consumed_energy):
         if image.dtype != np.uint8:
             image = image.astype(np.uint8)
 
@@ -68,10 +77,65 @@ class Renderer:
             self.base_thickness,
             cv2.LINE_AA,
         )
-        return im_time
 
-    def render_simple_image(self, image, state, time, leaderboard):
-        img = self.prepare_simple_image(image, state, time)
+        img = self.draw_energy_bar(im_time, consumed_energy)
+
+        return img
+
+    def draw_energy_bar(self, img, consumed_energy):
+            
+        im_energy = cv2.putText(
+            img,
+            f"Energy consumed {self.energy_unit}",
+            (50, self.consumed_energy_ys[0]-15),
+            self.font,
+            self.fontScale,
+            (0, 0, 0),
+            self.base_thickness,
+            cv2.LINE_AA,
+        )
+        # add scale (black empty rectangle with spines and 0, 100, 200, ..., 1000 as text)
+        img_energy = cv2.rectangle(
+            im_energy,
+            (50 - 2, self.consumed_energy_ys[0] - 2),
+            (50 + 2 + self.energy_bar_width, self.consumed_energy_ys[1] + 2),
+            (0, 0, 0),
+            2,
+        )
+        for i in range(self.energy_bar_graduations+1):
+            text = f"{i*self.energy_bar_graduation_increment+self.energy_bar_min}"
+            ((fw,fh), baseline) = cv2.getTextSize(text,
+                                                fontFace=self.font,
+                                                fontScale=0.5,
+                                                thickness=2) # empty string is good enough
+            img_energy = cv2.putText(
+                img_energy,
+                text,
+                (50 + i * self.energy_bar_width // self.energy_bar_graduations - fw//2, self.consumed_energy_ys[1] + fh+6),
+                self.font,
+                0.5,
+                (0, 0, 0),
+                1,
+                cv2.LINE_AA,
+            )
+        # add filled gree rectangle with consumed energy
+        consumed_energy_rescaled = consumed_energy*self.energy_mutlipler - self.energy_bar_min
+        consumed_energy_width = np.rint(self.energy_bar_width * consumed_energy_rescaled  / self.energy_bar_max).astype(int)
+        img_energy = cv2.rectangle(
+            img_energy,
+            (50, self.consumed_energy_ys[0]),
+            (50 + consumed_energy_width, self.consumed_energy_ys[1]),
+            (0, 255, 0),
+            -1,
+        )
+
+        if consumed_energy_rescaled > self.energy_bar_max:
+            self.energy_bar_min += self.energy_bar_max
+
+        return img_energy
+
+    def render_simple_image(self, image, state, time, leaderboard, consumed_energy):
+        img = self.prepare_simple_image(image, state, time, consumed_energy)
         img = self.add_leaderboard(img, leaderboard)
         cv2.imshow(self.window_name, img)
         cv2.waitKey(1)
@@ -94,7 +158,7 @@ class Renderer:
         )
 
     def render_countdown(self, base_img, state, countdown, leaderboard):
-        base_img = self.prepare_simple_image(base_img, state, 0)
+        base_img = self.prepare_simple_image(base_img, state, 0, 0)
 
         for i in range(countdown, 0, -1):
             img = self.put_centered_text(base_img, str(i), 10)
@@ -108,8 +172,8 @@ class Renderer:
         cv2.waitKey(1)  # 1ms just to update the window
         time.sleep(1)  # wait for 1 second
 
-    def render_finish_line_image(self, image, state, crossing_time, time, leaderboard):
-        img = self.prepare_simple_image(image, state, time)
+    def render_finish_line_image(self, image, state, crossing_time, time, leaderboard, consumed_energy):
+        img = self.prepare_simple_image(image, state, time, consumed_energy)
         img = self.put_centered_text(
             img, f"You are quite the fly: {crossing_time:.3f}s", 2
         )
@@ -144,8 +208,8 @@ class Renderer:
         
         return img
 
-
     def reset(self):
+        self.energy_bar_min = 0
         self.speed_list = np.zeros(self.speed_window_size)
 
     def quit(self):
