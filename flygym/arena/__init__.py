9ed27b7a882843cd0e3d139899a11be2e3f143ca
Dom <dominicd7@hotmail.com>
2024-12-18 10:11:47 +0100

    Remove arena

--- flygym/arena/__init__.py
+++ flygym/arena/__init__.py
@@ -2,4 +2,3 @@ from .base import BaseArena, FlatTerrain
 from .complex_terrain import GappedTerrain, BlocksTerrain, MixedTerrain
 from .sensory_environment import OdorArena
 from .tethered import Tethered
-from .game import SlalomArena
