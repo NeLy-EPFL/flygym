1c53d3d91982519772456e71b546728b7a96f07e
stimpfli <victor.stimpfling@epfl.ch>
2025-01-14 17:45:03 +0100

    black reformating

--- flygym/__init__.py
+++ flygym/__init__.py
@@ -1,6 +1,6 @@
 from .simulation import Simulation, SingleFlySimulation
 from .fly import Fly
-from .camera import Camera, NeckCamera
+from .camera import Camera, YawOnlyCamera, ZStabCamera, GravityAlignedCamera
 from .util import get_data_path, load_config
 
 from os import environ
