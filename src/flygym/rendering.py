352277bdef47c2743e7b13c8b27fbb084dba6d57
Sibo Wang <sibo.wang@epfl.ch>
2026-02-11 18:39:27 +0100

    change Renderer API to make it more similar to its warp counterpart

--- src/flygym/rendering.py
+++ src/flygym/rendering.py
@@ -1,81 +1,93 @@
 from multiprocessing import Process
 from pathlib import Path
+from typing import Any
 from os import PathLike
+from collections.abc import Sequence
 
-import mujoco
-import mujoco.viewer
+import mujoco as mj
+import mujoco.viewer as mjviewer
 import dm_control.mjcf as mjcf
 import mediapy
 import imageio.v3 as iio
 import numpy as np
 
-__all__ = ["Renderer", "launch_interactive_viewer", "preview_model"]
+__all__ = ["Renderer", "CameraSpec", "launch_interactive_viewer", "preview_model"]
+
+CameraSpec = str | int | mj.MjvCamera | mjcf.Element
 
 
 class Renderer:
-    """Manages rendering of MuJoCo simulations to video frames.
-
-    Automatically determines when to capture frames based on the specified output FPS
-    and playback speed, so that the user can call `render_as_needed` inside their
-    simulation loop without worrying about timing.
-
-    Args:
-        mj_model:
-            Compiled MuJoCo model to render.
-        height:
-            Frame height in pixels.
-        width:
-            Frame width in pixels.
-        playback_speed:
-            Playback speed multiplier (1.0 = real-time).
-        output_fps:
-            FPS of the output video. This takes the playback speed into account: for
-            example, if we want a 25-FPS video at 0.5x speed, frames will be rendered 50
-            times per simulated second (managed internally by this class).
-        camera:
-            Camera specification (name, ID, or MjvCamera object).
-        **kwargs:
-            Additional arguments passed to mujoco.Renderer.
-    """
-
     def __init__(
         self,
-        mj_model: mujoco.MjModel,
+        mj_model: mj.MjModel,
+        camera: CameraSpec | Sequence[CameraSpec],
         *,
-        height: int = 240,
-        width: int = 320,
+        camera_res: tuple[int, int] = (240, 320),
         playback_speed: float = 0.2,
         output_fps: int = 25,
-        camera: str | int | mujoco.MjvCamera | mjcf.Element = -1,
-        **kwargs,
+        **kwargs: Any,
     ):
-        self.mj_renderer = mujoco.Renderer(mj_model, height, width, **kwargs)
+        self.mj_model = mj_model
+        self._cameras_intern_id_lookup = self._resolve_camera_spec(camera)
+        self.camera_res = camera_res
+
+        nrows, ncols = camera_res
+        self.mj_renderer = mj.Renderer(mj_model, nrows, ncols, **kwargs)
+
         self.playback_speed = playback_speed
         self.output_fps = output_fps
-        self.frame_capture_interval_sec = 1 / (output_fps / playback_speed)
-        if isinstance(camera, mjcf.Element) and camera.tag == "camera":
-            camera = camera.full_identifier
-        self.camera = camera
-        self.last_render_time_sec = -np.inf
-        self.frames = []
+        self._secs_between_renders = 1 / (output_fps / playback_speed)
 
-    def render_as_needed(
-        self,
-        mj_data,
-        scene_option: mujoco.MjvOption | None = None,
-    ) -> np.ndarray | None:
-        """Render frame if enough time has elapsed since last render. Returns the
-        rendered frame as a numpy array, or None if no frame was rendered. The
-        `scene_option` argument is forwarded to `mujoco.Renderer.update_scene()`."""
-        curr_time = mj_data.time
-        if curr_time >= self.last_render_time_sec + self.frame_capture_interval_sec:
-            self.last_render_time_sec = curr_time
-            self.mj_renderer.update_scene(mj_data, self.camera, scene_option)
-            frame = self.mj_renderer.render()
-            self.frames.append(frame)
-            return frame
+        self._last_render_time_sec = -np.inf
+        self.frames = {cam_name: [] for cam_name in self._cameras_intern_id_lookup}
+
+    def _resolve_camera_spec(
+        self, spec: CameraSpec | Sequence[CameraSpec]
+    ) -> dict[str, mj.MjvCamera]:
+
+        def resolve_single_spec(s: CameraSpec) -> tuple[str, mj.MjvCamera]:
+            if isinstance(s, mj.MjvCamera):
+                internal_cam_id = s.fixedcamid
+                full_id = mj.mj_id2name(
+                    self.mj_model, mj.mjtObj.mjOBJ_CAMERA, internal_cam_id
+                )
+                return internal_cam_id, full_id
+            elif isinstance(s, str):
+                internal_cam_id = mj.mj_name2id(
+                    self.mj_model, mj.mjtObj.mjOBJ_CAMERA, s
+                )
+                return internal_cam_id, s
+            elif isinstance(s, mjcf.Element):
+                full_id = s.full_identifier
+                internal_cam_id = mj.mj_name2id(
+                    self.mj_model, mj.mjtObj.mjOBJ_CAMERA, full_id
+                )
+                return internal_cam_id, full_id
+            elif isinstance(s, int):
+                full_id = mj.mj_id2name(self.mj_model, mj.mjtObj.mjOBJ_CAMERA, s)
+                return s, full_id
+            else:
+                raise ValueError(
+                    f"Invalid camera spec: {s}. Must be one of str, int, "
+                    "mujoco.MjvCamera, or mjcf.Element."
+                )
+
+        resolved = {}
+        for s in spec if isinstance(spec, Sequence) else [spec]:
+            internal_cam_id, full_id = resolve_single_spec(s)
+            resolved[full_id] = internal_cam_id
+        return resolved
+
+    def render_as_needed(self, mj_data: mj.MjData) -> bool:
+        if mj_data.time >= self._last_render_time_sec + self._secs_between_renders:
+            self._last_render_time_sec = mj_data.time
+            for cam_name, internal_cam_id in self._cameras_intern_id_lookup.items():
+                self.mj_renderer.update_scene(mj_data, internal_cam_id)
+                frame = self.mj_renderer.render()
+                self.frames[cam_name].append(frame)
+            return True
         else:
-            return None
+            return False
 
     def close(self):
         self.mj_renderer.close()
@@ -87,29 +99,71 @@ class Renderer:
         self.close()
         return False  # don't suppress exceptions
 
-    def show_in_notebook(self, **kwargs) -> str | None:
-        if len(self.frames) == 0:
-            raise RuntimeError("No frames have been rendered yet.")
+    def show_in_notebook(
+        self, camera: CameraSpec | Sequence[CameraSpec] | None = None, **kwargs
+    ) -> None:
+        if camera is None:
+            camera_names = list(self._cameras_intern_id_lookup.keys())
+        else:
+            camera_names = list(self._resolve_camera_spec(camera).keys())
 
-        return mediapy.show_video(self.frames, fps=self.output_fps, **kwargs)
+        for cam_name in camera_names:
+            frames = self.frames[cam_name]
+            if len(frames) == 0:
+                raise RuntimeError(f"No frame recorded yet for camera {cam_name}.")
+            mediapy.show_video(frames, fps=self.output_fps, title=cam_name, **kwargs)
 
-    def save_video(self, output_path: PathLike, **kwargs) -> None:
-        if len(self.frames) == 0:
-            raise RuntimeError("No frames have been rendered yet.")
+    def save_video(
+        self,
+        output_path: dict[CameraSpec, PathLike] | PathLike,
+        **kwargs,
+    ) -> None:
+        if isinstance(output_path, str | Path):
+            output_path = Path(output_path)
+            if len(self._cameras_intern_id_lookup) == 1:
+                cam_name = list(self._cameras_intern_id_lookup.keys())[0]
+                path_by_camera = {cam_name: output_path}
+            else:
+                if not output_path.exists():
+                    output_path.mkdir(parents=True)
+                elif not output_path.is_dir():
+                    raise ValueError(
+                        "There are >1 cameras and `output_path` is given as a single "
+                        "path. In this case, `output_path` is treated as a directory "
+                        "and videos from all cameras will be saved under it. However, "
+                        f"the provided path {output_path} already exists and is not a "
+                        "directory."
+                    )
+                path_by_camera = {
+                    cam_name: output_path / f"{cam_name.replace('/', '-')}.mp4"
+                    for cam_name in self._cameras_intern_id_lookup
+                }
+        elif isinstance(output_path, dict):
+            camera_names = list(self._resolve_camera_spec(list(output_path)))
+            path_by_camera = {
+                cam_name: path
+                for cam_name, path in zip(camera_names, output_path.values())
+            }
+        else:
+            raise ValueError(
+                f"Invalid type of `output_path`: {type(output_path)}. Must be one of "
+                f"{str(Path)}, {dict}, or {str(Path)}."
+            )
 
-        output_path = Path(output_path)
-        output_path.parent.mkdir(parents=True, exist_ok=True)
-        iio.imwrite(
-            output_path, self.frames, fps=self.output_fps, codec="libx264", **kwargs
-        )
+        for cam_name, path in path_by_camera.items():
+            frames = self.frames[cam_name]
+            if len(frames) == 0:
+                raise RuntimeError(f"No frame recorded yet for camera {cam_name}.")
+            output_path.parent.mkdir(parents=True, exist_ok=True)
+            iio.imwrite(path, frames, fps=self.output_fps, codec="libx264", **kwargs)
 
     def reset(self):
-        self.frames = []
+        self.frames = {cam_name: [] for cam_name in self._cameras_intern_id_lookup}
 
 
 def launch_interactive_viewer(
-    mj_model: mujoco.MjModel,
-    mj_data: mujoco.MjData,
+    mj_model: mj.MjModel,
+    mj_data: mj.MjData,
     run_async: bool = False,
     init_keyframe: str | None = "neutral",
 ) -> None:
@@ -118,23 +172,25 @@ def launch_interactive_viewer(
     It should be set to True when launching from a Jupyter notebook."""
 
     if init_keyframe is not None:
-        key_id = mujoco.mj_name2id(mj_model, mujoco.mjtObj.mjOBJ_KEY, init_keyframe)
-        mujoco.mj_resetDataKeyframe(mj_model, mj_data, key_id)
+        key_id = mj.mj_name2id(mj_model, mj.mjtObj.mjOBJ_KEY, init_keyframe)
+        mj.mj_resetDataKeyframe(mj_model, mj_data, key_id)
 
     if run_async:
-        p = Process(target=mujoco.viewer.launch, args=(mj_model, mj_data))
+        p = Process(target=mj.viewer.launch, args=(mj_model, mj_data))
         p.start()
         # Don't join!
     else:
-        mujoco.viewer.launch(mj_model, mj_data)
+        mjviewer.launch(mj_model, mj_data)
 
 
 def preview_model(
-    mj_model: mujoco.MjModel,
-    mj_data: mujoco.MjData,
+    mj_model: mj.MjModel,
+    mj_data: mj.MjData,
     camera: mjcf.Element | str,
+    *,
     init_keyframe: str | None = "neutral",
     duration: float = 0.1,
+    camera_res: tuple[int, int] = (240, 320),
     playback_speed=0.1,
     output_fps: int = 25,
     show_in_notebook: bool = False,
@@ -142,20 +198,21 @@ def preview_model(
     **kwargs,
 ):
     if init_keyframe is not None:
-        key_id = mujoco.mj_name2id(mj_model, mujoco.mjtObj.mjOBJ_KEY, init_keyframe)
-        mujoco.mj_resetDataKeyframe(mj_model, mj_data, key_id)
+        key_id = mj.mj_name2id(mj_model, mj.mjtObj.mjOBJ_KEY, init_keyframe)
+        mj.mj_resetDataKeyframe(mj_model, mj_data, key_id)
 
     n_steps = int(duration / mj_model.opt.timestep)
 
     with Renderer(
         mj_model,
+        camera,
+        camera_res=camera_res,
         playback_speed=playback_speed,
         output_fps=output_fps,
-        camera=camera,
         **kwargs,
     ) as renderer:
         for step in range(n_steps):
-            mujoco.mj_step(mj_model, mj_data)
+            mj.mj_step(mj_model, mj_data)
             renderer.render_as_needed(mj_data)
 
         if show_in_notebook:
--- src/flygym/simulation.py
+++ src/flygym/simulation.py
@@ -1,15 +1,16 @@
 from collections import defaultdict
+from typing import Any
 
 import mujoco
 import dm_control.mjcf as mjcf
 import numpy as np
-from numpy.typing import NDArray
 from jaxtyping import Float
 from time import perf_counter_ns
+from collections.abc import Sequence
 
 from flygym.compose.fly import ActuatorType
 from flygym.compose.world import BaseWorld
-from flygym.rendering import Renderer
+from flygym.rendering import Renderer, CameraSpec
 from flygym.utils.profiling import print_perf_report
 
 
@@ -17,15 +18,21 @@ class Simulation:
     def __init__(self, world: BaseWorld) -> None:
         if len(world.fly_lookup) == 0:
             raise ValueError("The world must contain at least one fly.")
-        self.renderers: dict[str, Renderer] = {}
+        self.renderer: Renderer | None = None
         self.world = world
         self.mj_model, self.mj_data = world.compile()
 
-        # Map internal IDs in compiled MuJoCo model. This allows user to read to/write
-        # from body/joint/actuator in orders defined by Fly objects.
+        # Map internal IDs in the compiled MuJoCo model. This allows users to read from
+        # or write to body/joint/actuator in orders defined by Fly objects.
         self._map_internal_bodyids()
         self._map_internal_qposqveladrs()
         self._map_internal_actuator_ids()
+
+        # For performance profiling
+        self._curr_step = 0
+        self._frames_rendered = 0
+        self._total_physics_time_ns = 0
+        self._total_render_time_ns = 0
 
         # Reset everything (physics, renderers, and profiling stats)
         self.reset()
@@ -38,8 +45,8 @@ class Simulation:
         mujoco.mj_resetDataKeyframe(self.mj_model, self.mj_data, neutral_keyframe_id)
 
         # Reset renderers
-        for renderer in self.renderers.values():
-            renderer.reset()
+        if self.renderer is not None:
+            self.renderer.reset()
 
         # Stuff for performance profiling
         self._curr_step = 0
@@ -48,69 +55,48 @@ class Simulation:
         self._total_render_time_ns = 0
 
     def step(self) -> None:
-        # Step physics forward
         physics_start_ns = perf_counter_ns()
         mujoco.mj_step(self.mj_model, self.mj_data)
         poststep_start_ns = perf_counter_ns()
         self._total_physics_time_ns += poststep_start_ns - physics_start_ns
         self._curr_step += 1
 
-    def render_as_needed(self) -> dict[str, Float[NDArray, "height width 3"]]:
-        render_start_ns = perf_counter_ns()
-        images = {}
-        for name, renderer in self.renderers.items():
-            image = renderer.render_as_needed(self.mj_data)
-            if image is not None:
-                images[name] = image
-        render_finish_ns = perf_counter_ns()
-
-        # Update profiling stats
-        self._total_render_time_ns += render_finish_ns - render_start_ns
-        if images:
-            self._frames_rendered += 1
-
-        return images
-
-    def add_renderer(
+    def set_renderer(
         self,
-        name: str | None = None,
+        camera: CameraSpec | Sequence[CameraSpec],
         *,
-        height: int = 240,
-        width: int = 320,
+        camera_res: tuple[int, int] = (240, 320),
         playback_speed: float = 0.2,
         output_fps: int = 25,
-        camera: mjcf.Element | str | int | mujoco.MjvCamera = -1,
-        **kwargs,
-    ) -> str:
-        if isinstance(camera, mjcf.Element) and camera.tag == "camera":
-            camera = camera.full_identifier
-
-        renderer = Renderer(
+        **kwargs: Any,
+    ) -> Renderer:
+        self.renderer = Renderer(
             self.mj_model,
-            height=height,
-            width=width,
+            self.mj_data,
+            camera,
+            camera_res=camera_res,
             playback_speed=playback_speed,
             output_fps=output_fps,
-            camera=camera,
             **kwargs,
         )
-        name = f"renderer{len(self.renderers) + 1}" if name is None else name
-        self.renderers[name] = renderer
-        return name
+        return self.renderer
 
-    def get_joint_angles(self, fly_name: str) -> Float[NDArray, "n_jointdofs"]:
+    def render_as_needed(self) -> bool:
+        return self.renderer.render_as_needed(self.mj_data)
+
+    def get_joint_angles(self, fly_name: str) -> Float[np.ndarray, "n_jointdofs"]:
         internal_ids = self._intern_qposadrs_by_fly[fly_name]
         return self.mj_data.qpos[internal_ids]
 
-    def get_joint_velocities(self, fly_name: str) -> Float[NDArray, "n_jointdofs"]:
+    def get_joint_velocities(self, fly_name: str) -> Float[np.ndarray, "n_jointdofs"]:
         internal_ids = self._intern_qveladrs_by_fly[fly_name]
         return self.mj_data.qvel[internal_ids]
 
-    def get_body_positions(self, fly_name: str) -> Float[NDArray, "n_bodies 3"]:
+    def get_body_positions(self, fly_name: str) -> Float[np.ndarray, "n_bodies 3"]:
         internal_ids = self._internal_bodyids_by_fly[fly_name]
         return self.mj_data.xpos[internal_ids, :]
 
-    def get_body_rotations(self, fly_name: str) -> Float[NDArray, "n_bodies 4"]:
+    def get_body_rotations(self, fly_name: str) -> Float[np.ndarray, "n_bodies 4"]:
         internal_ids = self._internal_bodyids_by_fly[fly_name]
         return self.mj_data.xquat[internal_ids, :]
 
@@ -118,9 +104,9 @@ class Simulation:
         self,
         fly_name: str,
         actuator_type: ActuatorType,
-        inputs: Float[NDArray, "n_actuators"],
+        inputs: Float[np.ndarray, "n_actuators"],
     ):
-        internal_ids = self._intern_actuatorids_by_type_by_fly[fly_name][actuator_type]
+        internal_ids = self._intern_actuatorids_by_type_by_fly[actuator_type][fly_name]
         if len(inputs) != len(internal_ids):
             raise ValueError(
                 f"Expected {len(internal_ids)} inputs for actuator type "
@@ -168,10 +154,9 @@ class Simulation:
         }
 
     def _map_internal_actuator_ids(self) -> None:
-        self._intern_actuatorids_by_type_by_fly = {}
+        internal_actuatorids_by_fly_by_type = defaultdict(lambda: defaultdict(list))
 
         for fly_name, fly in self.world.fly_lookup.items():
-            ids_thisfly = defaultdict(list)
             for actuator_ty, actuators in fly.jointdof_to_mjcfactuator_by_type.items():
                 for jointdof, actuator_element in actuators.items():
                     internal_actuator_id = mujoco.mj_name2id(
@@ -179,12 +164,17 @@ class Simulation:
                         mujoco.mjtObj.mjOBJ_ACTUATOR,
                         actuator_element.full_identifier,
                     )
-                    ids_thisfly[actuator_ty].append(internal_actuator_id)
+                    internal_actuatorids_by_fly_by_type[actuator_ty][fly_name].append(
+                        internal_actuator_id
+                    )
 
-            ids_thisfly = {
-                ty: np.array(ids, dtype=np.int32) for ty, ids in ids_thisfly.items()
+        self._intern_actuatorids_by_type_by_fly = {
+            actuator_ty: {
+                fly_name: np.array(ids, dtype=np.int32)
+                for fly_name, ids in ids_by_fly.items()
             }
-            self._intern_actuatorids_by_type_by_fly[fly_name] = ids_thisfly
+            for actuator_ty, ids_by_fly in internal_actuatorids_by_fly_by_type.items()
+        }
 
     @property
     def time(self) -> float:
