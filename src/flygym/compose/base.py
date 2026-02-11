5d0595f1765177633dab2a117607877e4df6cbe8
Sibo Wang <sibo.wang@epfl.ch>
2026-02-11 18:37:41 +0100

    update dependencies to use bleeding-edge pre-release versions mujoco and mujoco_warp. Also implement hack in model compilation to relax version cross-dependency

--- src/flygym/compose/base.py
+++ src/flygym/compose/base.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from os import PathLike
 
-import mujoco
+import mujoco as mj
 import dm_control.mjcf as mjcf
 
 __all__ = ["BaseCompositionElement"]
@@ -17,13 +17,30 @@ class BaseCompositionElement(ABC):
     def mjcf_root(self) -> mjcf.RootElement:
         pass
 
-    def compile(self) -> tuple[mujoco.MjModel, mujoco.MjData]:
+    def compile(self) -> tuple[mj.MjModel, mj.MjData]:
         """Compile the MJCF model into MuJoCo MjModel and MjData objects. This is where
         `dm_control.mjcf` "hands off" the model that it composes to `mujoco` for
         simulation. Things like the ordering of generalized coordinates (qpos) are
         determined here."""
-        physics = mjcf.Physics.from_mjcf_model(self.mjcf_root)
-        return physics.model._model, physics.data._data
+        # physics = mjcf.Physics.from_mjcf_model(self.mjcf_root)
+        # return physics.model._model, physics.data._data
+
+        # Quick hack for relaxing MuJoCo/dm_control version dependency:
+        # We're using pre-release versions of MJWarp for the lateste features. These are
+        # not compatible with the latest stable release of dm_control, which is what we
+        # use for MJCF composition. The workaround is to not use dm_control's Physics
+        # wrapper at all, and instead export the MJCF model to XML and then load it
+        # directly with mujoco.
+
+        from tempfile import TemporaryDirectory
+
+        with TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            self.save_xml_with_assets(tmpdir, "model.xml")
+            mj_model = mj.MjModel.from_xml_path((tmpdir / "model.xml").as_posix())
+            mj_data = mj.MjData(mj_model)
+
+            return mj_model, mj_data
 
     def save_xml_with_assets(
         self, output_dir: PathLike, xml_filename: str = None
