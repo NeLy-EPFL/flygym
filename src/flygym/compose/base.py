ebb42d4a15899076a6f208b5144f6d7e2f7f6174
Sibo Wang <sibo.wang@epfl.ch>
2026-03-17 10:34:13 +0100

    refactor: restore physics model compilation in MJCF models - MuJoCo and dm_control schemas are now consistent

--- src/flygym/compose/base.py
+++ src/flygym/compose/base.py
@@ -22,25 +22,8 @@ class BaseCompositionElement(ABC):
         `dm_control.mjcf` "hands off" the model that it composes to `mujoco` for
         simulation. Things like the ordering of generalized coordinates (qpos) are
         determined here."""
-        # physics = mjcf.Physics.from_mjcf_model(self.mjcf_root)
-        # return physics.model._model, physics.data._data
-
-        # Quick hack for relaxing MuJoCo/dm_control version dependency:
-        # We're using pre-release versions of MJWarp for the lateste features. These are
-        # not compatible with the latest stable release of dm_control, which is what we
-        # use for MJCF composition. The workaround is to not use dm_control's Physics
-        # wrapper at all, and instead export the MJCF model to XML and then load it
-        # directly with mujoco.
-
-        from tempfile import TemporaryDirectory
-
-        with TemporaryDirectory() as tmpdir:
-            tmpdir = Path(tmpdir)
-            self.save_xml_with_assets(tmpdir, "model.xml")
-            mj_model = mj.MjModel.from_xml_path((tmpdir / "model.xml").as_posix())
-            mj_data = mj.MjData(mj_model)
-
-            return mj_model, mj_data
+        physics = mjcf.Physics.from_mjcf_model(self.mjcf_root)
+        return physics.model._model, physics.data._data
 
     def save_xml_with_assets(
         self, output_dir: PathLike, xml_filename: str = None
