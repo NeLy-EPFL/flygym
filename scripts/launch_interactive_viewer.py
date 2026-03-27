8e107aa500730fdc94eb5811022d4a631532b021
Sibo Wang <sibo.wang@epfl.ch>
2026-03-27 18:10:57 +0100

    reimplement pose axis order conversion

--- scripts/launch_interactive_viewer.py
+++ scripts/launch_interactive_viewer.py
@@ -1,6 +1,3 @@
-from pathlib import Path
-
-from flygym import assets_dir
 from flygym.anatomy import (
     Skeleton,
     JointPreset,
@@ -8,17 +5,18 @@ from flygym.anatomy import (
     ActuatedDOFPreset,
     ContactBodiesPreset,
 )
-from flygym.compose import Fly, ActuatorType, FlatGroundWorld, KinematicPose
+from flygym.compose import Fly, ActuatorType, FlatGroundWorld, KinematicPosePreset
 from flygym.rendering import launch_interactive_viewer
 from flygym.utils.math import Rotation3D
 
 joint_preset = JointPreset.ALL_BIOLOGICAL
 axis_order = AxisOrder.YAW_PITCH_ROLL
+# axis_order = AxisOrder.ROLL_PITCH_YAW
 actuated_dofs = ActuatedDOFPreset.LEGS_ACTIVE_ONLY
 actuator_type = ActuatorType.POSITION
 actuator_position_gain = 50.0
-neutral_pose_file = assets_dir / "model/pose/neutral.yaml"
-spawn_position = (0, 0, 0.7)  # xyz in mm
+neutral_pose = KinematicPosePreset.NEUTRAL
+spawn_position = (0, 0, 0.8)  # xyz in mm
 spawn_rotation = Rotation3D("quat", (1, 0, 0, 0))  # wxyz in quaternion
 bodysegs_with_ground_contact = ContactBodiesPreset.LEGS_THORAX_ABDOMEN_HEAD
 run_async = False  # might need to change to True if running launching from a notebook
@@ -28,7 +26,6 @@ def main():
     fly = Fly()
 
     skeleton = Skeleton(joint_preset=joint_preset, axis_order=axis_order)
-    neutral_pose = KinematicPose(path=neutral_pose_file)
     fly.add_joints(skeleton, neutral_pose)
 
     actuated_dofs_list = skeleton.get_actuated_dofs_from_preset(actuated_dofs)
@@ -53,7 +50,6 @@ def main():
 
     # Compile model and get data container
     mj_model, mj_data = world.compile()
-    world.save_xml_with_assets(Path("~/my_world_model/").expanduser())
 
     # Launch interactive viewer
     launch_interactive_viewer(mj_model, mj_data, run_async=run_async)
