a4c918af855d64fd5dfdcd3504ef0a3197956c57
Sibo Wang <sibo.wang@epfl.ch>
2026-03-28 15:18:37 +0100

    update tests, excluding warp module

--- tests/examples/test_motion_snippet.py
+++ tests/examples/test_motion_snippet.py
@@ -5,9 +5,8 @@ import numpy as np
 
 from flygym.anatomy import AxisOrder, JointPreset, ActuatedDOFPreset, Skeleton
 from flygym.compose.fly import Fly, ActuatorType
-from flygym.compose.pose import KinematicPose
+from flygym.compose.pose import KinematicPosePreset
 from flygym_examples.spotlight_data.preprocessing import MotionSnippet
-import flygym
 
 
 @pytest.fixture(scope="module")
@@ -17,7 +16,7 @@ def snippet():
 
 @pytest.fixture(scope="module")
 def fly_for_snippet():
-    neutral_pose = KinematicPose(path=flygym.assets_dir / "model/pose/neutral.yaml")
+    neutral_pose = KinematicPosePreset.NEUTRAL.get_pose_by_axis_order(AxisOrder.YAW_PITCH_ROLL)
     skeleton = Skeleton(
         axis_order=AxisOrder.YAW_PITCH_ROLL,
         joint_preset=JointPreset.LEGS_ONLY,
