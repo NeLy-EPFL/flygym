d451e5fdfe9d15a01c200ec527df562d0c5eb85c
Sibo Wang <sibo.wang@epfl.ch>
2026-03-30 00:25:49 +0200

    add gpu benchmarking code

---
+++ scripts/dev/run_gpu_benchmark.py
@@ -0,0 +1,24 @@
+from subprocess import run
+from pathlib import Path
+
+from flygym.warp.utils import check_gpu
+from flygym_demo.benchmark import run_benchmark
+
+if __name__ == "__main__":
+    SIM_TIMESTEP = 1e-4
+    OUTPUT_DIR = Path("gpu_benchmark_results/")
+
+    check_gpu()
+    df = run_benchmark(
+        enable_rendering=False,
+        min_worlds=2**4,  # 16
+        max_worlds=2**14,  # 16384
+        factor=2,
+        sim_timestep=SIM_TIMESTEP,
+        sim_steps=1000,
+    )
+
+    OUTPUT_DIR.mkdir(exist_ok=True)
+    df.to_csv(OUTPUT_DIR / "gpu_simulation_benchmark_results.csv", index=False)
+    with open(OUTPUT_DIR / "gpu_info.csv", "w") as f:
+        run(["nvidia-smi", "--query-gpu=name,memory.total", "--format=csv"], stdout=f)
